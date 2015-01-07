7b2df76d0bef3b9bac1a9a69ccb45e42d417b655
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2015-01-07 22:57:04 +0900

    add TypeChecker, Evaluator stub

---
+++ src/parser/TypeChecker.cpp
@@ -0,0 +1,59 @@
+/*
+ * TypeChecker.cpp
+ *
+ *  Created on: 2015/01/07
+ *      Author: skgchxngsxyz-osx
+ */
+
+#include "TypeChecker.h"
+
+TypeChecker::TypeChecker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TypeChecker::~TypeChecker() {
+	// TODO Auto-generated destructor stub
+}
+
+
+int TypeChecker::visitIntValueNode       (IntValueNode        *node) { return 0; } //TODO
+int TypeChecker::visitFloatValueNode     (FloatValueNode      *node) { return 0; } //TODO
+int TypeChecker::visitBooleanValueNode   (BooleanValueNode    *node) { return 0; } //TODO
+int TypeChecker::visitStringValueNode    (StringValueNode     *node) { return 0; } //TODO
+int TypeChecker::visitStringExprNode     (StringExprNode      *node) { return 0; } //TODO
+int TypeChecker::visitArrayNode          (ArrayNode           *node) { return 0; } //TODO
+int TypeChecker::visitMapNode            (MapNode             *node) { return 0; } //TODO
+int TypeChecker::visitPairNode           (PairNode            *node) { return 0; } //TODO
+int TypeChecker::visitSymbolNode         (SymbolNode          *node) { return 0; } //TODO
+int TypeChecker::visitIndexNode          (IndexNode           *node) { return 0; } //TODO
+int TypeChecker::visitAccessNode         (AccessNode          *node) { return 0; } //TODO
+int TypeChecker::visitCastNode           (CastNode            *node) { return 0; } //TODO
+int TypeChecker::visitInstanceOfNode     (InstanceOfNode      *node) { return 0; } //TODO
+int TypeChecker::visitApplyNode          (ApplyNode           *node) { return 0; } //TODO
+int TypeChecker::visitConstructorCallNode(ConstructorCallNode *node) { return 0; } //TODO
+int TypeChecker::visitCondOpNode         (CondOpNode          *node) { return 0; } //TODO
+int TypeChecker::visitProcessNode        (ProcessNode         *node) { return 0; } //TODO
+int TypeChecker::visitProcArgNode        (ProcArgNode         *node) { return 0; } //TODO
+int TypeChecker::visitSpecialCharNode    (SpecialCharNode     *node) { return 0; } //TODO
+int TypeChecker::visitTaskNode           (TaskNode            *node) { return 0; } //TODO
+int TypeChecker::visitInnerTaskNode      (InnerTaskNode       *node) { return 0; } //TODO
+int TypeChecker::visitAssertNode         (AssertNode          *node) { return 0; } //TODO
+int TypeChecker::visitBlockNode          (BlockNode           *node) { return 0; } //TODO
+int TypeChecker::visitBreakNode          (BreakNode           *node) { return 0; } //TODO
+int TypeChecker::visitContinueNode       (ContinueNode        *node) { return 0; } //TODO
+int TypeChecker::visitExportEnvNode      (ExportEnvNode       *node) { return 0; } //TODO
+int TypeChecker::visitImportEnvNode      (ImportEnvNode       *node) { return 0; } //TODO
+int TypeChecker::visitForNode            (ForNode             *node) { return 0; } //TODO
+int TypeChecker::visitForInNode          (ForInNode           *node) { return 0; } //TODO
+int TypeChecker::visitWhileNode          (WhileNode           *node) { return 0; } //TODO
+int TypeChecker::visitIfNode             (IfNode              *node) { return 0; } //TODO
+int TypeChecker::visitReturnNode         (ReturnNode          *node) { return 0; } //TODO
+int TypeChecker::visitThrowNode          (ThrowNode           *node) { return 0; } //TODO
+int TypeChecker::visitCatchNode          (CatchNode           *node) { return 0; } //TODO
+int TypeChecker::visitTryNode            (TryNode             *node) { return 0; } //TODO
+int TypeChecker::visitFinallyNode        (FinallyNode         *node) { return 0; } //TODO
+int TypeChecker::visitVarDeclNode        (VarDeclNode         *node) { return 0; } //TODO
+int TypeChecker::visitAssignNode         (AssignNode          *node) { return 0; } //TODO
+int TypeChecker::visitFunctionNode       (FunctionNode        *node) { return 0; } //TODO
+int TypeChecker::visitEmptyNode          (EmptyNode           *node) { return 0; } //TODO
---
+++ src/parser/TypeChecker.h
@@ -0,0 +1,63 @@
+/*
+ * TypeChecker.h
+ *
+ *  Created on: 2015/01/07
+ *      Author: skgchxngsxyz-osx
+ */
+
+#ifndef PARSER_TYPECHECKER_H_
+#define PARSER_TYPECHECKER_H_
+
+#include "../core/DSType.h"
+#include "../core/CalleeHandle.h"
+#include "../ast/Node.h"
+#include "../ast/NodeVisitor.h"
+
+class TypeChecker : public NodeVisitor {
+public:
+	TypeChecker();
+	virtual ~TypeChecker();
+
+	int visitIntValueNode       (IntValueNode        *node); // override
+	int visitFloatValueNode     (FloatValueNode      *node); // override
+	int visitBooleanValueNode   (BooleanValueNode    *node); // override
+	int visitStringValueNode    (StringValueNode     *node); // override
+	int visitStringExprNode     (StringExprNode      *node); // override
+	int visitArrayNode          (ArrayNode           *node); // override
+	int visitMapNode            (MapNode             *node); // override
+	int visitPairNode           (PairNode            *node); // override
+	int visitSymbolNode         (SymbolNode          *node); // override
+	int visitIndexNode          (IndexNode           *node); // override
+	int visitAccessNode         (AccessNode          *node); // override
+	int visitCastNode           (CastNode            *node); // override
+	int visitInstanceOfNode     (InstanceOfNode      *node); // override
+	int visitApplyNode          (ApplyNode           *node); // override
+	int visitConstructorCallNode(ConstructorCallNode *node); // override
+	int visitCondOpNode         (CondOpNode          *node); // override
+	int visitProcessNode        (ProcessNode         *node); // override
+	int visitProcArgNode        (ProcArgNode         *node); // override
+	int visitSpecialCharNode    (SpecialCharNode     *node); // override
+	int visitTaskNode           (TaskNode            *node); // override
+	int visitInnerTaskNode      (InnerTaskNode       *node); // override
+	int visitAssertNode         (AssertNode          *node); // override
+	int visitBlockNode          (BlockNode           *node); // override
+	int visitBreakNode          (BreakNode           *node); // override
+	int visitContinueNode       (ContinueNode        *node); // override
+	int visitExportEnvNode      (ExportEnvNode       *node); // override
+	int visitImportEnvNode      (ImportEnvNode       *node); // override
+	int visitForNode            (ForNode             *node); // override
+	int visitForInNode          (ForInNode           *node); // override
+	int visitWhileNode          (WhileNode           *node); // override
+	int visitIfNode             (IfNode              *node); // override
+	int visitReturnNode         (ReturnNode          *node); // override
+	int visitThrowNode          (ThrowNode           *node); // override
+	int visitCatchNode          (CatchNode           *node); // override
+	int visitTryNode            (TryNode             *node); // override
+	int visitFinallyNode        (FinallyNode         *node); // override
+	int visitVarDeclNode        (VarDeclNode         *node); // override
+	int visitAssignNode         (AssignNode          *node); // override
+	int visitFunctionNode       (FunctionNode        *node); // override
+	int visitEmptyNode          (EmptyNode           *node); // override
+};
+
+#endif /* PARSER_TYPECHECKER_H_ */
