310f09167c842a95b103b3fbc5790a158b911def
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2015-01-22 23:53:47 +0900

    add license notation to each source code

--- src/core/DSType.cpp
+++ src/core/DSType.cpp
@@ -1,10 +1,18 @@
 /*
- * DSType.cpp
+ * Copyright (C) 2015 Nagisa Sekiguchi
  *
- *  Created on: 2014/12/31
- *      Author: skgchxngsxyz-osx
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 
 #include "DSType.h"
 #include "DSObject.h"
