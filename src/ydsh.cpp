756b0cf9a2ecebc98fe3ac3052e93d412587ecf7
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2015-03-20 01:24:34 +0900

    move terminal

--- src/ydsh.cpp
+++ src/ydsh.cpp
@@ -15,6 +15,10 @@
  */
 
 #include <iostream>
+#include <histedit.h>
+#include <string>
+#include <string.h>
+
 #include <util/debug.h>
 #include <parser/Lexer.h>
 #include <parser/Parser.h>
@@ -23,10 +27,133 @@
 #include <core/TypePool.h>
 #include <parser/TypeChecker.h>
 #include <core/DSType.h>
-#include <exe/Terminal.h>
 #include <ast/Node.h>
 #include <ast/dump.h>
 using namespace std;
+
+static bool continuation = false;
+
+static char *prompt(EditLine *el) {
+    return continuation ? (char *) "> " : (char *) "test> ";
+}
+
+class Terminal {
+private:
+    EditLine *el;
+    History *ydsh_history;
+    HistEvent event;
+
+    /**
+     * contains previous read line.
+     */
+    std::string lineBuf;
+
+public:
+    Terminal(const char *progName);
+    ~Terminal();
+
+    /**
+     * not delete return value.
+     * return null if reach end of file or occurs error.
+     * skip white space and empty string.
+     */
+    const char *readLine();
+
+private:
+    const char *readLineImpl();
+    void addHistory();
+};
+
+
+Terminal::Terminal(const char *progName) :
+        el(0), ydsh_history(0), event(), lineBuf() {
+    this->el = el_init(progName, stdin, stdout, stderr);
+    el_set(this->el, EL_PROMPT, prompt);
+    el_set(this->el, EL_EDITOR, "emacs");
+
+    this->ydsh_history = history_init();
+    if(this->ydsh_history == 0) {
+        fatal("editline history initialization failed\n");
+    }
+
+    history(this->ydsh_history, &this->event, H_SETSIZE, 200);
+    el_set(el, EL_HIST, history, this->ydsh_history);
+}
+
+Terminal::~Terminal() {
+    history_end(this->ydsh_history);
+    el_end(this->el);
+}
+
+static inline bool isSkipLine(const char *line, int count) {
+    if(line == 0) {
+        return false;
+    }
+    for(int i = 0; i < count; i++) {
+        switch(line[i]) {
+        case ' ':
+        case '\t':
+        case '\r':
+        case '\n':
+            break;
+        default:
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool checkLineContinuation(const char *line) {
+    if(line == 0) {
+        return false;
+    }
+    unsigned int size = strlen(line);
+    for(unsigned int i = 0; i < size; i++) {
+        if(line[i] == '\\' && i + 1 < size && line[i + 1] == '\n') {
+            return true;
+        }
+    }
+    return false;
+}
+
+const char *Terminal::readLine() {
+    this->lineBuf = std::string();
+    const char *line;
+    do {
+        line = this->readLineImpl();
+        if(line == 0) {
+            return line;
+        }
+        this->lineBuf += line;
+        continuation = true;
+    } while(checkLineContinuation(line));
+
+    continuation = false;
+    this->addHistory();
+    return this->lineBuf.c_str();
+}
+
+const char *Terminal::readLineImpl() {
+    int count;
+    const char *line;
+
+    do {
+        line = el_gets(this->el, &count);
+    } while(isSkipLine(line, count));
+    return line;
+}
+
+void Terminal::addHistory() {
+    std::string target("\\\n");
+    std::string buf(this->lineBuf);
+    std::string::size_type pos = buf.find(target);
+    while(pos != std::string::npos) {
+        buf.replace(pos, target.size(), "");
+        pos = buf.find(target, pos);
+    }
+    history(this->ydsh_history, &this->event, H_ENTER, buf.c_str());
+}
+
 
 int main(int argc, char **argv) {
     Terminal term(argv[0]);
