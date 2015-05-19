ca3c837f8cb83e80171727f6ccbd9c1300695a75
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2015-05-19 17:19:28 +0900

    add execution test

---
+++ test/Exec/exec_test.cpp
@@ -0,0 +1,85 @@
+#include <gtest/gtest.h>
+
+#include <exe/Shell.h>
+
+#include <dirent.h>
+
+#ifndef EXEC_TEST_DIR
+#define EXEC_TEST_DIR "."
+#endif
+
+#ifndef EXEC_TEST_BIN
+#define EXEC_TEST_BIN "./ydsh"
+#endif
+
+static std::vector<const char *> getFileList(const char *path) {
+    std::vector<const char *> fileList;
+
+    DIR *dir = opendir(path);
+    if(dir == nullptr) {
+        exit(1);
+    }
+
+    dirent *entry;
+
+    do {
+        entry = readdir(dir);
+        if(entry == nullptr) {
+            break;
+        }
+        if(entry->d_type == DT_REG) {
+            fileList.push_back(entry->d_name);
+        }
+    } while(true);
+
+    return fileList;
+}
+
+class ExecTest : public ::testing::TestWithParam<const char *> {
+private:
+    std::string targetName;
+
+public:
+    ExecTest() : targetName() {
+    }
+
+    virtual void SetUp() {
+        this->targetName += EXEC_TEST_DIR;
+        this->targetName += "/";
+        this->targetName += this->GetParam();
+    }
+
+    virtual void TearDown() {
+        this->targetName.clear();
+    }
+
+    virtual const std::string &getSourceName() {
+        return this->targetName;
+    }
+
+    virtual void doTest() {
+        SCOPED_TRACE("");
+
+        std::string cmd;
+        cmd += EXEC_TEST_BIN;
+        cmd += " ";
+        cmd += this->getSourceName();
+
+        int status = system(cmd.c_str());
+        ASSERT_EQ(0, status);
+    }
+};
+
+TEST_P(ExecTest, baseTest) {
+    ASSERT_NO_FATAL_FAILURE({
+        SCOPED_TRACE("");
+        this->doTest();
+    });
+}
+
+INSTANTIATE_TEST_CASE_P(ExecTest, ExecTest, ::testing::ValuesIn(getFileList(EXEC_TEST_DIR)));
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
