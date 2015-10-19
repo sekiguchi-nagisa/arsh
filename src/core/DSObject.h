6140c95e5262d6de635b83ac3a3ef6ddb22c99ca
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2015-10-20 00:55:59 +0900

    fix compiler warning in Apple clang 7.0.0

--- src/core/DSObject.h
+++ src/core/DSObject.h
@@ -209,8 +209,9 @@ inline T *typeAs(const DSValue &value) noexcept {
     if(useSafeCast) {
         auto *r = dynamic_cast<T*>(value.get());
         if(r == nullptr) {
+            DSObject &v = *value;
             std::cerr << "target type is: " << misc::Demangle()(typeid(T))
-            << ", but actual is: " << misc::Demangle()(typeid(*value)) << std::endl;
+            << ", but actual is: " << misc::Demangle()(typeid(v)) << std::endl;
             abort();
         }
         return r;
