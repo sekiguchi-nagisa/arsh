a8279c6f793426bb87fa8ea5cbdf381359be727a
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2016-07-27 22:32:47 +0900

    add lexer fuzzer stub

---
+++ fuzz/lexer_fuzzer.cpp
@@ -0,0 +1,16 @@
+//
+// Created by skgchxngsxyz-carbon on 16/07/27.
+//
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include <lexer.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    using namespace ydsh;
+
+//    Lexer lexer;
+
+    return 0;
+}
