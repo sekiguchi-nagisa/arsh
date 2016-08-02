b6c36823248396f5e5136543080559a196dde583
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2016-08-02 23:36:24 +0900

    fix lexer_fuzzer

--- fuzzing/lexer_fuzzer.cpp
+++ fuzzing/lexer_fuzzer.cpp
@@ -10,7 +10,20 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     using namespace ydsh;
 
-//    Lexer lexer;
+    LexerMode modes[] = {
+            yycSTMT, yycEXPR, yycNAME, yycDSTRING, yycTYPE, yycCMD
+    };
 
+    for(auto mode : modes) {
+        Lexer lexer("<dummy>", reinterpret_cast<const char *>(data), size);
+        lexer.pushLexerMode(mode);
+
+        TokenKind kind;
+
+        do {
+            Token token;
+            kind = lexer.nextToken(token);
+        } while(kind != EOS && kind != INVALID);
+    }
     return 0;
 }
