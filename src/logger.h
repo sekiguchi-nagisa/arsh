7a4f9221dfc0da94bf520e3b92d56abe0c317a4f
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2016-10-07 22:54:54 +0900

    rewrite logger

--- src/logger.h
+++ src/logger.h
@@ -18,8 +18,120 @@
 #define YDSH_LOGGER_H
 
 #include <config.h>
-#include "misc/logger_base.hpp"
 
-DEFINE_LOGGING_POLICY("YDSH_", "APPENDER", TRACE_TOKEN, DUMP_EXEC, TRACE_SIGNAL, DUMP_CONSOLE);
+#ifdef USE_LOGGING
+
+#include <unistd.h>
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <ctime>
+
+#include "misc/flag_util.hpp"
+
+#define EACH_LOGGING_POLICY(E) \
+    E(TRACE_TOKEN) \
+    E(DUMP_EXEC) \
+    E(TRACE_SIGNAL) \
+    E(DUMP_CONSOLE)
+
+
+namespace __detail_logger {
+
+enum LoggingPolicy : unsigned int {
+#define GEN_ENUM(E) E,
+    EACH_LOGGING_POLICY(GEN_ENUM)
+#undef GEN_ENUM
+};
+
+class Logger {
+private:
+    std::ostream *stream_;
+
+    unsigned int whiteList_;
+
+public:
+    ~Logger() {
+        if(this->stream_ != &std::cerr) {
+            delete this->stream_;
+        }
+    }
+
+private:
+    Logger() : stream_(nullptr), whiteList_(0) {
+        // init appender
+        const char *appender = getenv("YDSH_APPENDER");
+        if(appender != nullptr) {
+            std::ostream *os = new std::ofstream(appender);
+            if(!(*os)) {
+                delete os;
+                os = nullptr;
+            }
+            this->stream_ = os;
+        }
+        if(this->stream_ == nullptr) {
+            this->stream_ = &std::cerr;
+        }
+
+        // init policy
+        const char *policies[] = {
+#define GEN_STR(E) #E,
+                EACH_LOGGING_POLICY(GEN_STR)
+#undef GEN_STR
+        };
+
+        unsigned int index = 0;
+        for(auto &p : policies) {
+            const char *env = getenv(p);
+            if(env != nullptr) {
+                ydsh::setFlag(this->whiteList_, (1u << index));
+            }
+            index++;
+        }
+    }
+
+    bool checkPolicy(LoggingPolicy policy) const {
+        return ydsh::hasFlag(this->whiteList_, 1u << static_cast<unsigned int>(policy));
+    }
+
+    void writeHeader(const char *funcName) {
+        time_t timer = time(nullptr);
+        struct tm *local = localtime(&timer);
+
+        char buf[32];
+        strftime(buf, 32, "%F %T", local);
+
+        *this->stream_ << buf << " ["  << getpid() << "] " << funcName << "(): ";
+    }
+
+public:
+    template <typename FUNC>
+    void operator()(LoggingPolicy policy, const char *funcName, FUNC func) {
+        if(this->checkPolicy(policy)) {
+            this->writeHeader(funcName);
+            func(*this->stream_);
+            *this->stream_ << std::endl;
+        }
+    }
+
+    static Logger &get() {
+        static Logger logger;
+        return logger;
+    }
+};
+
+} // namespace __detail_logger
+
+
+#define LOG_L(P, L) __detail_logger::Logger::get()(__detail_logger::P, __func__, L)
+#define LOG(P, V) LOG_L(P, [&](std::ostream & ___s) { ___s << V; })
+
+#else
+
+#define LOG_L(P, L)
+#define LOG(P, L) 
+
+#endif
 
 #endif //YDSH_LOGGER_H
