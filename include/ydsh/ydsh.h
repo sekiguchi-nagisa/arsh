55439231b60e916ed5cdd5c8148a02c81a8d72f7
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2017-01-04 12:04:12 +0900

    rename option in public header

--- include/ydsh/ydsh.h
+++ include/ydsh/ydsh.h
@@ -67,14 +67,14 @@ void DSState_setArguments(DSState *st, char *const *args);
 
 
 /* for option */
-#define DS_OPTION_DUMP_UAST   ((unsigned int) (1 << 0))
-#define DS_OPTION_DUMP_AST    ((unsigned int) (1 << 1))
-#define DS_OPTION_DUMP_CODE   ((unsigned int) (1 << 2))
-#define DS_OPTION_PARSE_ONLY  ((unsigned int) (1 << 3))
-#define DS_OPTION_ASSERT      ((unsigned int) (1 << 4))
-#define DS_OPTION_TOPLEVEL    ((unsigned int) (1 << 5))
-#define DS_OPTION_TRACE_EXIT  ((unsigned int) (1 << 6))
-#define DS_OPTION_INTERACTIVE ((unsigned int) (1 << 7))
+#define DS_OPTION_DUMP_UAST  ((unsigned int) (1 << 0))
+#define DS_OPTION_DUMP_AST   ((unsigned int) (1 << 1))
+#define DS_OPTION_DUMP_CODE  ((unsigned int) (1 << 2))
+#define DS_OPTION_PARSE_ONLY ((unsigned int) (1 << 3))
+#define DS_OPTION_ASSERT     ((unsigned int) (1 << 4))
+#define DS_OPTION_TOPLEVEL   ((unsigned int) (1 << 5))
+#define DS_OPTION_TRACE_EXIT ((unsigned int) (1 << 6))
+#define DS_OPTION_HISTORY    ((unsigned int) (1 << 7))
 
 unsigned int DSState_option(const DSState *st);
 void DSState_setOption(DSState *st, unsigned int optionSet);
