0f9045a2eaa9943468da341fd2a8272f47c3f67c
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2017-01-13 20:26:36 +0900

    now lcov report C1 coverage

--- report_cov.sh
+++ report_cov.sh
@@ -41,6 +41,6 @@ fi
 
 # generate coverage report
 OUTPUT="coverage_report"
-lcov $TOOL --directory . --capture --output-file ${OUTPUT}.info
-lcov --remove ${OUTPUT}.info 'test/*' 'tools/*' 'ext/*' 'fuzzing/*' '/usr/include/*' 'nextToken.re2c.cpp' --output-file ${OUTPUT}-cleaned.info
-genhtml -o ${OUTPUT} ${OUTPUT}-cleaned.info
+lcov --rc lcov_branch_coverage=1 $TOOL --directory . --capture --output-file ${OUTPUT}.info
+lcov --rc lcov_branch_coverage=1 --remove ${OUTPUT}.info 'test/*' 'tools/*' 'ext/*' 'fuzzing/*' '/usr/include/*' 'nextToken.re2c.cpp' --output-file ${OUTPUT}-cleaned.info
+genhtml --rc lcov_branch_coverage=1 -o ${OUTPUT} ${OUTPUT}-cleaned.info
