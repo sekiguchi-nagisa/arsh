dc7e48d680444d1df4d51df7e161a234b95994e1
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2017-03-01 22:52:30 +0900

    add test case for destructor call of local variable

--- test/vm/vm_test.cpp
+++ test/vm/vm_test.cpp
@@ -69,10 +69,20 @@ protected:
         this->inspector.setBreakOp(op);
     }
 
-    DSValue getGlobal(const char *varName) const {
-        auto *handle = this->state->symbolTable.lookupHandle(varName);
-        unsigned int index = handle->getFieldIndex();
-        return state->getGlobal(index);
+    void resetBreakPoint() {
+        this->setBreakPoint(OpCode::NOP);
+    }
+
+    void RefCount(const char *gvarName, unsigned int refCount) {
+        ASSERT_NO_FATAL_FAILURE(ASSERT_TRUE(gvarName != nullptr));
+
+        auto *handle = this->state->symbolTable.lookupHandle(gvarName);
+        ASSERT_NO_FATAL_FAILURE(ASSERT_TRUE(handle != nullptr));
+
+        auto &v = this->state->getGlobal(handle->getFieldIndex());
+        ASSERT_NO_FATAL_FAILURE(ASSERT_TRUE(v.isObject()));
+
+        ASSERT_NO_FATAL_FAILURE(ASSERT_EQ(refCount, v.get()->getRefcount()));
     }
 };
 
@@ -81,6 +91,82 @@ TEST_F(VMTest, base) {
     this->eval("12");
     ASSERT_NO_FATAL_FAILURE(ASSERT_EQ(12, typeAs<Int_Object>(this->state->peek())->getValue()));
 }
+
+TEST_F(VMTest, deinit1) {
+    this->eval("var a = new [Int]()");
+    ASSERT_(RefCount("a", 1));
+
+    this->eval("{ var b = $a}");
+    ASSERT_(RefCount("a", 1));
+
+    this->setBreakPoint(OpCode::RAND);
+    this->eval("{ var b = $a; if $true { var c = $a }; $RANDOM; }");
+    ASSERT_(RefCount("a", 2));
+}
+
+//TEST_F(VMTest, deinit2) {  //FIXME:
+//    this->eval("{ var b = $@; throw 34; }");
+//    ASSERT_(RefCount("@", 1));
+//
+//    this->eval("{ var a = $@; { var b = $@; throw 34; }}");
+//    ASSERT_(RefCount("@", 1));
+//}
+
+TEST_F(VMTest, deinit3) {
+    this->eval("var i = 0; while $i < 2 { var b = $@; $i++ }");
+    ASSERT_(RefCount("@", 1));
+
+    this->eval("while $true { var b = $@; break; }");
+    ASSERT_(RefCount("@", 1));
+
+//    this->eval("do { var b = $@; var c = $b; continue; } while $b.size() > 0");   //FIXME:
+//    ASSERT_(RefCount("@", 1));
+
+    this->eval("for(var i = $@; $true;) { var b = $i; break; }");
+    ASSERT_(RefCount("@", 1));
+
+    this->eval("for(var i = 0; $i < 3; $i++) { var b = $@; continue; }");
+    ASSERT_(RefCount("@", 1));
+}
+
+TEST_F(VMTest, deinit4) {
+    this->setBreakPoint(OpCode::RAND);
+    this->eval("function f($a : [String]) { $RANDOM; var b = $a; }; $f($@)");
+    ASSERT_(RefCount("@", 2));
+}
+
+TEST_F(VMTest, deinit5) {
+    this->setBreakPoint(OpCode::RAND);
+    this->eval("function f($a : [String]) { var b = $a; { var c = $b } var c = $b; }; $f($@)");
+    ASSERT_(RefCount("@", 1));
+}
+
+TEST_F(VMTest, deinit6) {
+    this->setBreakPoint(OpCode::RAND);
+    this->eval("function f($a : [String]) { var b = $a; { var c = $b } var c = $b; }; $f($@)");
+    ASSERT_(RefCount("@", 1));
+}
+
+TEST_F(VMTest, deinit7) {
+    this->eval("try { var a = $@; 34 / 0; var b = $a; } catch($e) {}");
+    ASSERT_(RefCount("@", 1));
+
+    this->setBreakPoint(OpCode::RAND);
+    this->eval("try { while $true { var a = $@; break; } } finally {  $RANDOM; }");
+    ASSERT_(RefCount("@", 1));
+}
+
+TEST_F(VMTest, deinit8) {
+    this->setBreakPoint(OpCode::RAND);
+    this->eval("try { var a = $@; 34 / 0 } catch $e { $RANDOM; }");
+    ASSERT_(RefCount("@", 1));
+}
+
+//TEST_F(VMTest, deinit9) { //FIXME:
+//    this->setBreakPoint(OpCode::RAND);
+//    this->eval("try { var a = $@; 34 / 0 } catch $e { var b = $@; throw 34; } finally {  $RANDOM; }");
+//    ASSERT_(RefCount("@", 1));
+//}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
