4c4e92db09a497c81399d6e9e7ff7e3537fa046f
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2017-08-19 01:48:25 +0900

    fix fuzzer

--- fuzzing/ds_fuzzer.cpp
+++ fuzzing/ds_fuzzer.cpp
@@ -8,7 +8,7 @@
 #include <ydsh/ydsh.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    auto *state = DSState_createWithOption(DS_OPTION_COMPILE_ONLY);
+    auto *state = DSState_createWithMode(DS_EXEC_MODE_COMPILE_ONLY);
     DSError dsError;
     DSState_eval(state, "<dummy>", (const char *)data, size, &dsError);
     DSState_delete(&state);
