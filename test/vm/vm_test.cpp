76c7c27e234ed178412f563e0c90824c8ff08bb7
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2017-10-10 23:41:27 +0900

    improve vm test case

--- test/vm/vm_test.cpp
+++ test/vm/vm_test.cpp
@@ -12,19 +12,28 @@ class VMInspector : public VMHook {
 private:
     OpCode breakOp;
     BreakPointHandler handler;
+    bool called;
 
 public:
-    VMInspector() : breakOp(OpCode::HALT), handler() {}
+    VMInspector() : breakOp(OpCode::HALT), handler(), called(true) {}
 
     void setHandler(OpCode op, BreakPointHandler &&handler) {
         this->breakOp = op;
         this->handler = std::move(handler);
+        if(this->handler) {
+            this->called = false;
+        }
+    }
+
+    bool getCalled() const {
+        return this->called;
     }
 
     void vmFetchHook(DSState &, OpCode op) override {
         if(this->breakOp == op) {
             if(this->handler) {
                 this->handler();
+                this->called = true;
             }
         }
     }
@@ -60,6 +69,7 @@ protected:
         DSError e;
         DSState_eval(this->state, "(dummy)", code, strlen(code), &e);
         ASSERT_NO_FATAL_FAILURE(ASSERT_EQ(kind, e.kind));
+        ASSERT_NO_FATAL_FAILURE(ASSERT_TRUE(this->inspector.getCalled()));
     }
 
     void eval(const char *code, DSErrorKind kind, OpCode breakOp, BreakPointHandler &&handler) {
@@ -89,97 +99,98 @@ protected:
 };
 
 TEST_F(VMTest, base) {
-    this->eval("12", DS_ERROR_KIND_SUCCESS, OpCode::POP, [&]{
+    ASSERT_(this->eval("12", DS_ERROR_KIND_SUCCESS, OpCode::POP, [&]{
         ASSERT_NO_FATAL_FAILURE(ASSERT_EQ(12, typeAs<Int_Object>(this->state->peek())->getValue()));
-    });
+    }));
 }
 
 TEST_F(VMTest, deinit1) {
-    this->eval("var a = new [Int]()");
+    ASSERT_(this->eval("var a = new [Int]()"));
     ASSERT_(RefCount("a", 1));
 
-    this->eval("{ var b = $a}");
+    ASSERT_(this->eval("{ var b = $a}"));
     ASSERT_(RefCount("a", 1));
 
-    this->eval("{ var b = $a; if $true { var c = $a }; $RANDOM; }", DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&]{
+    ASSERT_(this->eval("{ var b = $a; if $true { var c = $a }; $RANDOM; }", DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&]{
         ASSERT_(RefCount("a", 2));
-    });
+    }));
 }
 
 TEST_F(VMTest, deinit2) {
-    this->eval("{ var b = $@; throw 34; }", DS_ERROR_KIND_RUNTIME_ERROR);
+    ASSERT_(this->eval("{ var b = $@; throw 34; }", DS_ERROR_KIND_RUNTIME_ERROR));
     ASSERT_(RefCount("@", 1));
 
-    this->eval("{ var a = $@; { var b = $@; var c = $b; throw 34; }}", DS_ERROR_KIND_RUNTIME_ERROR);
+    ASSERT_(this->eval("{ var a = $@; { var b = $@; var c = $b; throw 34; }}", DS_ERROR_KIND_RUNTIME_ERROR));
     ASSERT_(RefCount("@", 1));
 }
 
 TEST_F(VMTest, deinit3) {
-    this->eval("var i = 0; while $i < 2 { var b = $@; $i++ }");
+    ASSERT_(this->eval("var i = 0; while $i < 2 { var b = $@; $i++ }"));
     ASSERT_(RefCount("@", 1));
 
-    this->eval("while $true { var b = $@; break; }");
+    ASSERT_(this->eval("while $true { var b = $@; break; }"));
     ASSERT_(RefCount("@", 1));
 
-    this->eval("for(var i = $@; $true;) { var b = $i; break; }");
+    ASSERT_(this->eval("for(var i = $@; $true;) { var b = $i; break; }"));
     ASSERT_(RefCount("@", 1));
 
-    this->eval("for(var i = 0; $i < 3; $i++) { var b = $@; continue; }");
+    ASSERT_(this->eval("for(var i = 0; $i < 3; $i++) { var b = $@; continue; }"));
     ASSERT_(RefCount("@", 1));
 }
 
 TEST_F(VMTest, deinit4) {
-    this->eval("function f($a : [String]) { $RANDOM; var b = $a; }; $f($@)", DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&]{
-        ASSERT_(RefCount("@", 2));
-    });
+    ASSERT_(this->eval("function f($a : [String]) { $RANDOM; var b = $a; }; $f($@)",
+                       DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&] {
+                ASSERT_(RefCount("@", 2));
+            }));
 }
 
 TEST_F(VMTest, deinit5) {
-    this->eval("function f($a : [String]) { var b = $a; { var c = $b; $RANDOM; }; var c = $b; }; $f($@)",
-               DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&]{
+    ASSERT_(this->eval("function f($a : [String]) { var b = $a; { var c = $b; $RANDOM; }; var c = $b; }; $f($@)",
+                       DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&]{
                 ASSERT_(RefCount("@", 4));
-            });
+            }));
 }
 
 TEST_F(VMTest, deinit6) {
-    this->eval("function f($a : [String]) { var b = $a; { var c = $b }; $RANDOM; var c = $b; }; $f($@)",
-               DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&] {
+    ASSERT_(this->eval("function f($a : [String]) { var b = $a; { var c = $b }; $RANDOM; var c = $b; }; $f($@)",
+                       DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&] {
                 ASSERT_(RefCount("@", 3));
-            });
+            }));
 }
 
 TEST_F(VMTest, deinit7) {
-    this->eval("try { var a = $@; 34 / 0; var b = $a; } catch($e) {}");
+    ASSERT_(this->eval("try { var a = $@; 34 / 0; var b = $a; } catch($e) {}"));
     ASSERT_(RefCount("@", 1));
 
-    this->eval("try { while $true { var a = $@; break; } } finally {  $RANDOM; }", DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&]{
-        ASSERT_(RefCount("@", 1));
-    });
+    ASSERT_(this->eval("try { while $true { var a = $@; break; } } finally {  $RANDOM; }",
+                       DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&]{
+                ASSERT_(RefCount("@", 1));
+            }));
 }
 
 TEST_F(VMTest, deinit8) {
-    this->eval("try { var a = $@; 34 / 0 } catch $e { $RANDOM; }", DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&]{
+    ASSERT_(this->eval("try { var a = $@; 34 / 0 } catch $e { $RANDOM; }", DS_ERROR_KIND_SUCCESS, OpCode::RAND, [&]{
         ASSERT_(RefCount("@", 1));
-    });
+    }));
 }
 
 TEST_F(VMTest, deinit9) {
-    this->eval("try { var a = $@; 34 / 0 } catch $e { var b = $@; throw 34; } finally {  $RANDOM; }",
-               DS_ERROR_KIND_RUNTIME_ERROR, OpCode::RAND, [&]{
+    ASSERT_(this->eval("try { var a = $@; 34 / 0 } catch $e { var b = $@; throw 34; } finally {  $RANDOM; }",
+                    DS_ERROR_KIND_RUNTIME_ERROR, OpCode::RAND, [&]{
                 ASSERT_(RefCount("@", 1));
-            });
+            }));
 }
 
 TEST_F(VMTest, deinit10) {
-    this->eval("try { var a = $@; var b = $a; 34 / 0 } catch $e : Int { var b = $@; var c = $b; var d = $c; } finally {  $RANDOM; }",
-               DS_ERROR_KIND_RUNTIME_ERROR, OpCode::RAND, [&]{
+    ASSERT_(this->eval("try { var a = $@; var b = $a; 34 / 0 } catch $e : Int { var b = $@; var c = $b; var d = $c; } finally {  $RANDOM; }",
+                       DS_ERROR_KIND_RUNTIME_ERROR, OpCode::RAND, [&]{
                 ASSERT_(RefCount("@", 1));
-            });
+            }));
 }
 
 TEST_F(VMTest, sig1) {
-    this->eval("function f($s : Signal) {}");
-//    auto *func = this->getFuncObject("f");
+    ASSERT_(this->eval("function f($s : Signal) {}"));
     auto func = this->getValue("f");
     ASSERT_NO_FATAL_FAILURE(ASSERT_TRUE(func != nullptr));
 
