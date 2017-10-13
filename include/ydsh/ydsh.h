8566a000793be959a71c381d80daaff9d1cdca76
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2017-10-13 23:21:44 +0900

    cleanup public header

--- include/ydsh/ydsh.h
+++ include/ydsh/ydsh.h
@@ -31,9 +31,9 @@ typedef struct DSState DSState;
 /*********************/
 
 typedef enum {
-    DS_EXEC_MODE_NORMAL = 0,
-    DS_EXEC_MODE_PARSE_ONLY = 1,
-    DS_EXEC_MODE_CHECK_ONLY = 2,
+    DS_EXEC_MODE_NORMAL       = 0,
+    DS_EXEC_MODE_PARSE_ONLY   = 1,
+    DS_EXEC_MODE_CHECK_ONLY   = 2,
     DS_EXEC_MODE_COMPILE_ONLY = 3,
 } DSExecMode;
 
@@ -104,11 +104,11 @@ void DSState_setDumpTarget(DSState *st, DSDumpKind kind, FILE *fp);
 
 
 /* for option */
-#define DS_OPTION_ASSERT       ((unsigned short) (1 << 1))
-#define DS_OPTION_TOPLEVEL     ((unsigned short) (1 << 2))
-#define DS_OPTION_TRACE_EXIT   ((unsigned short) (1 << 3))
-#define DS_OPTION_HISTORY      ((unsigned short) (1 << 4))
-#define DS_OPTION_INTERACTIVE  ((unsigned short) (1 << 5))
+#define DS_OPTION_ASSERT       ((unsigned short) (1 << 0))
+#define DS_OPTION_TOPLEVEL     ((unsigned short) (1 << 1))
+#define DS_OPTION_TRACE_EXIT   ((unsigned short) (1 << 2))
+#define DS_OPTION_HISTORY      ((unsigned short) (1 << 3))
+#define DS_OPTION_INTERACTIVE  ((unsigned short) (1 << 4))
 
 unsigned short DSState_option(const DSState *st);
 void DSState_setOption(DSState *st, unsigned short optionSet);
