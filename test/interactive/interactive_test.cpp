576d6d62592fe2236befcf466811898089c91f2f
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-01-04 15:49:57 +0900

    fix interactive test

--- test/interactive/interactive_test.cpp
+++ test/interactive/interactive_test.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
 
+#include <fstream>
+
 #include <misc/files.h>
+#include <misc/num.h>
+#include "../test_common.h"
 
 #ifndef INTERACTIVE_TEST_DIR
 #error require INTERACTIVE_TEST_DIR
@@ -25,6 +29,28 @@ static std::vector<std::string> getSortedFileList(const char *dir) {
     return ret;
 }
 
+static int extractStatus(const std::string &fileName, int defaultValue) {
+    std::ifstream input(fileName);
+    if(!input) {
+        fatal("broken file: %s\n", fileName.c_str());
+    }
+
+    for(std::string line; std::getline(input, line);) {
+        const char prefix[] = "#@status: ";
+        auto *ptr = strstr(line.c_str(), prefix);
+        if(ptr == line.c_str()) {
+            const char *str = ptr + strlen(prefix);
+            int s;
+            int v = convertToInt64(str, s);
+            if(s != 0) {
+                fatal("broken number: %s\n", str);
+            }
+            return v;
+        }
+    }
+    return defaultValue;
+}
+
 class InteractiveTest : public ::testing::TestWithParam<std::string> {
 private:
     std::string targetName;
@@ -42,15 +68,15 @@ public:
     virtual void doTest() {
         SCOPED_TRACE("");
 
-        std::string cmd("expect ");
-        cmd += this->targetName;
-        cmd += " ";
-        cmd += BIN_PATH;
-        cmd += " ";
-        cmd += INTERACTIVE_TEST_WORK_DIR;
-
-        int status = system(cmd.c_str());
-        ASSERT_EQ(0, status);
+        ProcBuilder builder ={
+                "expect",
+                this->targetName.c_str(),
+                BIN_PATH,
+                INTERACTIVE_TEST_WORK_DIR
+        };
+        auto pair = inspectStatus(builder.exec());
+        ASSERT_EQ(WaitType::EXITED, pair.second);
+        ASSERT_EQ(extractStatus(this->targetName, 0), pair.first);
     }
 };
 
