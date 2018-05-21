089b2bb99138a6c93b20de872f042becf0b6eb1d
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-05-21 23:44:27 +0900

    remove unused code

--- src/diagnosis.h
+++ src/diagnosis.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2017 Nagisa Sekiguchi
+ * Copyright (C) 2016-2018 Nagisa Sekiguchi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,10 +40,6 @@ public:
 
     const std::string &getMessage() const {
         return this->message;
-    }
-
-    bool operator==(const TypeLookupError &e) const {
-        return this->message == e.getMessage();
     }
 
     friend std::string extract(TypeLookupError &&e) {
@@ -112,10 +108,6 @@ public:
 
     const std::string &getMessage() const {
         return this->message;
-    }
-
-    bool operator==(const TypeCheckError &e) const {
-        return this->message == e.getMessage();
     }
 };
 
