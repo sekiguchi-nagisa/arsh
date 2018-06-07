19a66d51a8654a90d0d248bbda155823340de7ac
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-06-07 11:00:57 +0900

    fix build error in gcc

--- test/common/common_test.cpp
+++ test/common/common_test.cpp
@@ -73,7 +73,7 @@ TEST_F(ProcTest, pty) {
         return 1;
     });
     std::string str = "hello";
-    write(handle.in(), str.c_str(), str.size());
+    (void)write(handle.in(), str.c_str(), str.size());
     auto ret2 = handle.waitAndGetResult(false);
     ASSERT_NO_FATAL_FAILURE(this->expect(ret2, 0, WaitStatus::EXITED, "hello\n"));
 }
