efbbbef5e798ec92d6392ba4335f06add01eaef2
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-07-07 15:12:52 +0900

    update script

--- tools/scripts/asan.sh
+++ tools/scripts/asan.sh
@@ -3,13 +3,14 @@
 SCRIPT_DIR="$(cd $(dirname $0) && pwd -P)"
 ROOT=$SCRIPT_DIR/../..
 
-mkdir -p build-asan
+mkdir -p build-asan/app
 cd build-asan
 cmake $ROOT -G Ninja \
             -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
             -DCMAKE_BUILD_TYPE=debug \
-            -DSANITIZER=address
+            -DSANITIZER=address \
+            -DCMAKE_INSTALL_PREFIX=./app
 
-ninja
+ninja && ninja install
 
 ctest
