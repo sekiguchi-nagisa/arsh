31ab77ec42f8a6fd6e442c915b399230d860ff53
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-07-16 23:58:36 +0900

    fix poll interval

--- test/common/common_test.cpp
+++ test/common/common_test.cpp
@@ -111,12 +111,12 @@ TEST_F(ProcTest, pty3) {
     });
     auto r = write(handle.in(), "p", 1);
     (void) r;
-    auto output = handle.readAll(5);
+    auto output = handle.readAll(20);
     ASSERT_NO_FATAL_FAILURE(ASSERT_EQ("print\n", output.first));
 
     r = write(handle.in(), "x", 1);
     (void) r;
-    output = handle.readAll(5);
+    output = handle.readAll(20);
     ASSERT_NO_FATAL_FAILURE(ASSERT_EQ("ignore x\n", output.first));
 
     r = write(handle.in(), "b", 1);
