9602d570cebce9eb885c71b81b923e4813d95798
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-08-14 01:16:02 +0900

    Revert "temporary disable some result test case"
    This reverts commit 44df5bb5ff3790276e642731f6dae2a3b63a3ccd.

--- test/result/result_test.cpp
+++ test/result/result_test.cpp
@@ -103,19 +103,19 @@ Result<std::string, int> func(int index) {
     return Ok(std::to_string(index));
 }
 
-//TEST(result, result) {
-//    Result<std::string, int> ret = Ok(std::string("hello"));
-//    ASSERT_NO_FATAL_FAILURE(ASSERT_TRUE(ret));
-//    ASSERT_NO_FATAL_FAILURE(ASSERT_EQ("hello", ret.asOk()));
-//
-//    Result<std::string, int> ret2 = Err(12);
-//    ASSERT_NO_FATAL_FAILURE(ASSERT_FALSE(ret2));
-//    ASSERT_NO_FATAL_FAILURE(ASSERT_EQ(12, ret2.asErr()));
-//
-//    auto v = std::move(func(12).asOk());
-//    ASSERT_NO_FATAL_FAILURE(ASSERT_EQ("12", v));
-//    ASSERT_NO_FATAL_FAILURE(ASSERT_EQ(-1, std::move(func(-1).asErr())));
-//}
+TEST(result, result) {
+    Result<std::string, int> ret = Ok(std::string("hello"));
+    ASSERT_NO_FATAL_FAILURE(ASSERT_TRUE(ret));
+    ASSERT_NO_FATAL_FAILURE(ASSERT_EQ("hello", ret.asOk()));
+
+    Result<std::string, int> ret2 = Err(12);
+    ASSERT_NO_FATAL_FAILURE(ASSERT_FALSE(ret2));
+    ASSERT_NO_FATAL_FAILURE(ASSERT_EQ(12, ret2.asErr()));
+
+    auto v = std::move(func(12).asOk());
+    ASSERT_NO_FATAL_FAILURE(ASSERT_EQ("12", v));
+    ASSERT_NO_FATAL_FAILURE(ASSERT_EQ(-1, std::move(func(-1).asErr())));
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
