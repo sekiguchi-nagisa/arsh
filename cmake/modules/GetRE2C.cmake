8533319ade2c188db109f1b1d8b4d3b388cf732f
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-08-18 17:15:28 +0900

    remove re2c submodule. now use cmake external project

---
+++ cmake/modules/GetRE2C.cmake
@@ -0,0 +1,29 @@
+
+include(DownloadProject)
+
+macro(getRE2C)
+    if (CMAKE_VERSION VERSION_LESS 3.2)
+        set(UPDATE_DISCONNECTED_IF_AVAILABLE "")
+    else()
+        set(UPDATE_DISCONNECTED_IF_AVAILABLE "UPDATE_DISCONNECTED 1")
+    endif()
+
+    download_project(
+            PROJ                re2c
+            SOURCE_DIR          ${CMAKE_SOURCE_DIR}/re2c-source
+            DOWNLOAD_DIR        ${CMAKE_SOURCE_DIR}/re2c-download
+            BINARY_DIR          ${CMAKE_SOURCE_DIR}/re2c-build
+            GIT_REPOSITORY      https://github.com/skvadrik/re2c.git
+            GIT_TAG             1.0.3
+            ${UPDATE_DISCONNECTED_IF_AVAILABLE}
+    )
+
+    set(RE2C_BIN "${re2c_BINARY_DIR}/re2c")
+
+    if(NOT EXISTS "${re2c_SOURCE_DIR}/re2c/configure")
+        execute_process(COMMAND ./autogen.sh WORKING_DIRECTORY ${re2c_SOURCE_DIR}/re2c)
+    endif()
+
+    execute_process(COMMAND ${re2c_SOURCE_DIR}/re2c/configure WORKING_DIRECTORY ${re2c_BINARY_DIR})
+    execute_process(COMMAND make WORKING_DIRECTORY ${re2c_BINARY_DIR})
+endmacro()
--- tools/scripts/report_cov.sh
+++ tools/scripts/report_cov.sh
@@ -42,5 +42,5 @@ fi
 # generate coverage report
 OUTPUT="coverage_report"
 lcov --rc lcov_branch_coverage=1 $TOOL --directory . --capture --output-file ${OUTPUT}.info
-lcov --rc lcov_branch_coverage=1 --remove ${OUTPUT}.info '*test/*' '*tools/*' '*ext/*' '*fuzzing/*' '*googletest-source/*'  '/usr/include/*' '*src/nextToken.re2c.cpp' '*nextToken.cpp' '*v1/*' --output-file ${OUTPUT}-cleaned.info
+lcov --rc lcov_branch_coverage=1 --remove ${OUTPUT}.info '*test/*' '*tools/*' '*ext/*' '*fuzzing/*' '*googletest-source/*' '*re2c-source/*' '/usr/include/*' '*src/nextToken.re2c.cpp' '*nextToken.cpp' '*v1/*' --output-file ${OUTPUT}-cleaned.info
 genhtml --rc lcov_branch_coverage=1 -o ${OUTPUT} ${OUTPUT}-cleaned.info
