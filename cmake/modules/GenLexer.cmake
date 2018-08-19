2fa9eade782ffb1b05e2ac66d3d0dc6616d0634d
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-08-19 17:02:55 +0900

    add GenLexer.cmake module

---
+++ cmake/modules/GenLexer.cmake
@@ -0,0 +1,42 @@
+#
+# USAGE:
+#
+#   add_gen_lexer_target(TARGET        gen_lexer
+#                        SOURCE_FILE   ${CMAKE_CURRENT_SOURCE_DIR}/nextToken.re2c
+#                        OUTPUT_FILE   ${CMAKE_CURRENT_BINARY_DIR}/nextToken.cpp
+#                        RE2C_OPTION   "-W -Werror -c -8 -s -t ${CMAKE_CURRENT_BINARY_DIR}/src/yycond.h"
+#   )
+#
+
+include(CMakeParseArguments)
+
+function(add_gen_lexer_target)
+    set(single_args
+            TARGET
+            SOURCE_FILE
+            OUTPUT_FILE
+            RE2C_OPTION)
+
+    cmake_parse_arguments(GL_ARGS "" "${single_args}" "" ${ARGN})
+
+    # check argumnets
+    if(NOT GL_ARGS_TARGET)
+        message(FATAL_ERROR "require TARGET")
+    endif()
+
+    if(NOT GL_ARGS_SOURCE_FILE)
+        message(FATAL_ERROR "require SOURCE_FILE")
+    endif()
+
+    if(NOT GL_ARGS_OUTPUT_FILE)
+        message(FATAL_ERROR "require OUTPUT_FILE")
+    endif()
+
+    set(lexer_src ${GL_ARGS_OUTPUT_FILE})
+    set(option ${GL_ARGS_RE2C_OPTION})
+    separate_arguments(option)
+
+    add_custom_command(OUTPUT ${lexer_src}
+            COMMAND ${RE2C_BIN} ${option} -o ${lexer_src} ${GL_ARGS_SOURCE_FILE})
+    add_custom_target(${GL_ARGS_TARGET} DEPENDS ${lexer_src})
+endfunction()
