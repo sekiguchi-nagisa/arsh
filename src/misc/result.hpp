f631b6cf28e8ecabf34159f17a9951a719db7b48
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-11-09 22:21:08 +0900

    some fix

--- src/misc/result.hpp
+++ src/misc/result.hpp
@@ -247,7 +247,7 @@ public:
 
     template <typename U, typename F = __detail::resolvedType<U, T...>>
     Union(U &&value) noexcept : tag_(Tag<F>::value) {
-        this->value_.obtain(std::forward<U>(value));
+        this->value_.obtain(std::move(value));
     }
 
     Union(Union &&value) noexcept : tag_(value.tag()) {
