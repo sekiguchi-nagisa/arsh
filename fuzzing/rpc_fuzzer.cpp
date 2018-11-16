10e94bfbf9b35589bb97f414d7684aaacf3829f3
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-11-17 01:13:12 +0900

    add json rpc fuzzer

---
+++ fuzzing/rpc_fuzzer.cpp
@@ -0,0 +1,22 @@
+//
+// Created by skgchxngsxyz-carbon on 18/11/16.
+//
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "jsonrpc.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    rpc::RequestParser parser;
+    parser.append(reinterpret_cast<const char *>(data), size);
+    auto ret = parser();
+    if(parser.hasError()) {
+        FILE *fp = fopen("/dev/null", "w");
+        parser.showError(fp);
+        fclose(fp);
+    }
+    (void) ret;
+    return 0;
+}
+
