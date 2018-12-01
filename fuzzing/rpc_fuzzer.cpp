f5fc43da7d419e7d15968cf0b08bb1d7cfcf25e9
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2018-12-01 22:22:20 +0900

    fix build error

--- fuzzing/rpc_fuzzer.cpp
+++ fuzzing/rpc_fuzzer.cpp
@@ -8,7 +8,10 @@
 #include "jsonrpc.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    rpc::RequestParser parser;
+    using namespace ydsh;
+    using namespace rpc;
+
+    RequestParser parser;
     parser.append(reinterpret_cast<const char *>(data), size);
     auto ret = parser();
     if(parser.hasError()) {
