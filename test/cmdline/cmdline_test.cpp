f85a0c934e0b1c697995f2a70cb96f55ba0bedaf
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2019-02-08 23:14:59 +0900

    update test case

--- test/cmdline/cmdline_test.cpp
+++ test/cmdline/cmdline_test.cpp
@@ -813,10 +813,14 @@ ArithmeticError: zero division
     ASSERT_NO_FATAL_FAILURE(this->expect(DS(src), 1, "receive error: 4: 1\n", e));
 }
 
-TEST_F(CmdlineTest, signal) {
+TEST_F(CmdlineTest, signal1) {
     std::string str = strsignal(SIGKILL);
     str += "\n";
     ASSERT_NO_FATAL_FAILURE(this->expect(DS("sh -c 'kill -s kill $$'"), 128 + SIGKILL, "", str.c_str()));
+}
+
+TEST_F(CmdlineTest, signal2) {
+    ASSERT_NO_FATAL_FAILURE(this->expect(DS("cat /dev/random | grep 2> /dev/null"), 2));
 }
 
 struct CmdlineTest2 : public CmdlineTest, public TempFileFactory {
