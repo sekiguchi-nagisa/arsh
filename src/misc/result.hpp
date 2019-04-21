d0b368f80dec15b4ce45790116b0fae6133e0053
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2019-04-21 21:58:50 +0900

    add workaround for gcc5

--- src/misc/result.hpp
+++ src/misc/result.hpp
@@ -249,7 +249,7 @@ private:
 
 public:
     template <typename R>
-    static constexpr auto TAG = TypeTag<R, T...>::value;
+    static constexpr int TAG = TypeTag<R, T...>::value; // workaround for gcc5
 
     Union() noexcept : tag_(-1) {}
 
