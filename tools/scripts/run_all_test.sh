98d552aa9766bbef17868786b8dccfc4706f2a07
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2019-06-22 12:24:43 +0900

    fix test script

--- tools/scripts/run_all_test.sh
+++ tools/scripts/run_all_test.sh
@@ -1,9 +1,12 @@
+#!/bin/sh
+
 mkdir -p build-all
 cd build-all
 cmake .. -G Ninja -DCMAKE_CXX_COMPILER=clang++ -DUSE_EXTRA_TEST=on
 
 ninja
+sudo ninja install
 
-./ydsh ../tools/scripts/copy_mod4extra.ds
+../tools/scripts/copy_mod4extra.ds
 
 exec ctest --output-on-failure
