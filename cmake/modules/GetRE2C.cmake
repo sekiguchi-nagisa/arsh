376ca00d690ae9579d8d4ac3971d5ba2f8f2ef21
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2019-07-20 22:20:19 +0900

    fix download porject source dir

--- cmake/modules/GetRE2C.cmake
+++ cmake/modules/GetRE2C.cmake
@@ -10,9 +10,6 @@ macro(getRE2C)
 
     download_project(
             PROJ                re2c
-            SOURCE_DIR          ${CMAKE_SOURCE_DIR}/re2c-source
-            DOWNLOAD_DIR        ${CMAKE_SOURCE_DIR}/re2c-download
-            BINARY_DIR          ${CMAKE_SOURCE_DIR}/re2c-build
             GIT_REPOSITORY      https://github.com/skvadrik/re2c.git
             GIT_TAG             1.1.1
             GIT_PROGRESS        1
