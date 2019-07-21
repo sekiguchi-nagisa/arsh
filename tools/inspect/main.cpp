5e70ebba7b495a311c699fd5f57a16782f66e142
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2019-07-21 17:22:14 +0900

    add helper util for process status inspection

---
+++ tools/inspect/main.cpp
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2019 Nagisa Sekiguchi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <unistd.h>
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#include <misc/opt.hpp>
+#include <misc/fatal.h>
+
+#include <signals.h>
+
+using namespace ydsh;
+
+#define EACH_OPT(OP) \
+    OP(OUT,   "--out",   opt::HAS_ARG, "specify output file. default is stdout") \
+    OP(OUT2,  "-o",      opt::HAS_ARG, "equivalent to '--out'") \
+    OP(HELP,  "--help",  opt::NO_ARG,  "show help message") \
+    OP(HELP2,  "-h",     opt::NO_ARG,  "equivalent to '--help'")
+
+enum class OptionSet : unsigned int {
+#define GEN_ENUM(E, S, F, D) E,
+    EACH_OPT(GEN_ENUM)
+#undef GEN_ENUM
+};
+
+static std::ostream &format(std::ostream &stream) {
+    return stream << std::left << std::setw(8) << std::setfill(' ');
+}
+
+static void showPIDs(std::ostream &stream) {
+    stream << "+++++  PIDs  +++++" << std::endl;
+
+    struct {
+        const char *name;
+        long id;
+    } lists[] = {
+            {"PID", getpid()},
+            {"PPID", getppid()},
+            {"PGID", getpgrp()},
+            {"SID", getsid(0)},
+    };
+    for(auto &e : lists) {
+        stream << format << e.name << " => " << e.id << std::endl;
+    }
+    stream << std::endl;
+}
+
+static void showPGroup(std::ostream &stream) {
+    stream << "+++++  foreground process group  +++++" << std::endl;
+
+    stream << format << "STDIN" << " => " << (isatty(STDIN_FILENO) ? "TTY" : "NOTTY") << std::endl;
+    stream << format << "STDOUT" << " => " << (isatty(STDOUT_FILENO) ? "TTY" : "NOTTY") << std::endl;
+    stream << format << "STDERR" << " => " << (isatty(STDERR_FILENO) ? "TTY" : "NOTTY") << std::endl;
+
+    errno = 0;
+    auto pid = tcgetpgrp(STDIN_FILENO);
+    int errNum = errno;
+    stream << format << "TCPGID" << " => " << pid << std::endl;
+    stream << format << "errno" << " => " << errNum << std::endl;
+    stream << std::endl;
+}
+
+static void showSignals(std::ostream &stream) {
+    auto lists = getUniqueSignalList();
+    stream << "+++++  signal handler setting  +++++" << std::endl;
+    for(auto &e : lists) {
+        stream << format << getSignalName(e) << " => ";
+        struct sigaction action{};
+        sigaction(e, nullptr, &action);
+        if(action.sa_handler == SIG_DFL) {
+            stream << "SIG_DFL";
+        } else if(action.sa_handler == SIG_IGN) {
+            stream << "SIG_IGN";
+        } else {
+            stream << std::hex << reinterpret_cast<uintptr_t>(action.sa_handler);
+        }
+        stream << std::endl;
+    }
+    stream << std::endl;
+}
+
+static void showInfo(std::ostream &stream) {
+    showPIDs(stream);
+    showPGroup(stream);
+    showSignals(stream);
+}
+
+int main(int argc, char **argv) {
+    opt::Parser<OptionSet> parser = {
+#define GEN_OPT(E, S, F, D) {OptionSet::E, S, (F), D},
+            EACH_OPT(GEN_OPT)
+#undef GEN_OPT
+    };
+
+    const char *output = nullptr;
+
+    char **begin = argv + 1;
+    char **end = argv + argc;
+    opt::Result<OptionSet> result;
+    while((result = parser(begin, end))) {
+        switch(result.value()) {
+        case OptionSet::OUT:
+        case OptionSet::OUT2:
+            output = result.arg();
+            break;
+        case OptionSet::HELP:
+        case OptionSet::HELP2:
+            parser.printOption(stdout);
+            exit(0);
+        }
+    }
+    if(result.error() != opt::END) {
+        fprintf(stderr, "%s\n", result.formatError().c_str());
+        parser.printOption(stderr);
+        exit(1);
+    }
+
+    if(output) {
+        std::ofstream stream(output);
+        if(!stream) {
+            fatal_perror("cannot open file: %s", output);
+        }
+        showInfo(stream);
+    } else {
+        showInfo(std::cout);
+    }
+    return 0;
+}
--- tools/scripts/report_cov.sh
+++ tools/scripts/report_cov.sh
@@ -45,6 +45,7 @@ lcov --rc lcov_branch_coverage=1 $TOOL --directory . --capture --output-file ${O
 lcov --rc lcov_branch_coverage=1 --remove ${OUTPUT}.info \
     '*test/*' '*fuzzing/*' \
     '*-src/*' '/usr/include/*' '*v1/*' \
-    '*.re2c.cpp' '*nextToken.cpp' '*tools/json/lexer.cpp' '*tools/uri/uri_parser.cpp' '*tools/process/ansi.cpp'  \
+    '*.re2c.cpp' '*nextToken.cpp' '*tools/json/lexer.cpp' '*tools/uri/uri_parser.cpp' \
+    '*tools/process/ansi.cpp' '*tools/inspect/*' \
     --output-file ${OUTPUT}-cleaned.info
 genhtml --rc lcov_branch_coverage=1 -o ${OUTPUT} ${OUTPUT}-cleaned.info
