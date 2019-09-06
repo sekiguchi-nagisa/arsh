a06ef16d9498665b0c6a4397301f093a0daff9ec
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2019-09-06 20:07:15 +0900

    fix number parsing of test command

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -853,13 +853,13 @@ static int builtin_test(DSState &, Array_Object &argvObj) {
         case BinaryOp::LE:
         case BinaryOp::GE: {
             int s = 0;
-            long n1 = convertToInt64(left, s, 10);
+            long n1 = convertToInt64(left, s);
             if(s != 0) {
                 ERROR(argvObj, "%s: must be integer", left);
                 return 2;
             }
 
-            long n2 = convertToInt64(right, s, 10);
+            long n2 = convertToInt64(right, s);
             if(s != 0) {
                 ERROR(argvObj, "%s: must be integer", right);
                 return 2;
