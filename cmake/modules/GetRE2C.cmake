4fca13a7557a6d006ebb4409654c458e452cdf46
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2019-12-15 12:58:56 +0900

    update re2c to 1.3

--- cmake/modules/GetRE2C.cmake
+++ cmake/modules/GetRE2C.cmake
@@ -11,7 +11,7 @@ macro(getRE2C)
     download_project(
             PROJ                re2c
             GIT_REPOSITORY      https://github.com/skvadrik/re2c.git
-            GIT_TAG             1.2.1
+            GIT_TAG             1.3
             GIT_PROGRESS        1
     )
 
