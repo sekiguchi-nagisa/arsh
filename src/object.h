b2eb7ec6f22086e9f18ac40c7e35bfe4a9c1b721
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2020-01-06 23:05:28 +0900

    change String_Object#compare definition

--- src/object.h
+++ src/object.h
@@ -450,7 +450,11 @@ public:
     bool compare(const DSValue &obj) const override {
         auto *str2 = typeAs<String_Object>(obj);
         unsigned int size = std::min(this->size(), str2->size());
-        return memcmp(this->getValue(), str2->getValue(), size + 1) < 0;
+        int ret = memcmp(this->getValue(), str2->getValue(), size);
+        if(ret) {
+            return ret < 0;
+        }
+        return this->size() < str2->size();
     }
 
     size_t hash() const override;
