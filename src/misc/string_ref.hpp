eac5e0b61846553d060d233d2a035eb6b886d2a0
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2020-01-07 23:58:43 +0900

    add StringRef

---
+++ src/misc/string_ref.hpp
@@ -0,0 +1,197 @@
+/*
+ * Copyright (C) 2020 Nagisa Sekiguchi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef YDSH_MISC_STRING_REF_HPP
+#define YDSH_MISC_STRING_REF_HPP
+
+#include <string>
+#include <cstring>
+#include <cassert>
+
+#include "hash.hpp"
+
+namespace ydsh {
+
+/**
+ * similar to std::string_view/llvm::StringRef
+ */
+template <bool T>
+class StringRefBase {
+public:
+    static_assert(T, "not allow instantiation");
+
+    using size_type = std::size_t;
+
+    static constexpr const size_type npos = -1;
+
+private:
+    const char *ptr_{nullptr};
+    size_type size_{0};
+
+public:
+    StringRefBase() noexcept : ptr_(nullptr), size_(0) {}
+
+    StringRefBase(const StringRefBase &ref) noexcept = default;
+
+    /**
+     *
+     * @param value
+     * may be null. if not null, must be null-terminated string.
+     */
+    StringRefBase(const char *value) : ptr_(value), size_(0) { //NOLINT
+        if(this->ptr_) {
+            this->size_ = strlen(this->ptr_);
+        }
+    }
+
+    StringRefBase(const char *value, size_type size) noexcept : ptr_(value), size_(size) {}
+
+    StringRefBase(const std::string &value) noexcept : ptr_(value.c_str()), size_(value.size()) {} //NOLINT
+
+    size_type size() const {
+        return this->size_;
+    }
+
+    bool empty() const {
+        return this->size() == 0;
+    }
+
+    const char *data() const {
+        return this->ptr_;
+    }
+
+    const char *take() {
+        const char *tmp = nullptr;
+        std::swap(this->ptr_, tmp);
+        this->size_ = 0;
+        return tmp;
+    }
+
+    int compare(StringRefBase ref) const noexcept {
+        size_t size = std::min(this->size_, ref.size_);
+        int ret = memcmp(this->ptr_, ref.ptr_, size);
+        if(ret) {
+            return ret;
+        }
+        if(this->size_ < ref.size_) {
+            return -1;
+        }
+        return this->size_ == ref.size_ ? 0 : 1;
+    }
+
+    const char *begin() const {
+        return this->ptr_;
+    }
+
+    const char *end() const {
+        return this->ptr_ + this->size_;
+    }
+
+    char operator[](size_type index) const {
+        assert(index < this->size_);
+        return this->ptr_[index];
+    }
+
+    char front() const {
+        return (*this)[0];
+    }
+
+    char back() const {
+        return (*this)[this->size_ - 1];
+    }
+
+    /**
+     *
+     * @param startIndex
+     * inclusive
+     * @param stopIndex
+     * exclusive
+     * @return
+     */
+    StringRefBase slice(size_type startIndex, size_t stopIndex) const {
+        assert(startIndex <= stopIndex);
+        assert(stopIndex <= this->size_);
+        assert(startIndex < this->size_);
+
+        return StringRefBase(this->ptr_ + startIndex, stopIndex - startIndex);
+    }
+
+    size_type find(StringRefBase ref, size_type pos = 0) const {
+        if(pos > this->size_) {
+            return npos;
+        }
+        if(ref.size_ == 0) {
+            return pos;
+        }
+        auto *ret = memmem(this->ptr_ + pos, this->size_ - pos, ref.ptr_, ref.size_);
+        return ret != nullptr ? static_cast<const char *>(ret) - this->ptr_ : npos;
+    }
+
+    size_type indexOf(StringRefBase ref) const {
+        return this->find(ref, 0);
+    }
+
+    size_type lastIndexOf(StringRefBase ref) const {
+        size_type ret = npos;
+        size_type pos = 0;
+        do {
+            auto tmp = this->find(ref, pos);
+            if(tmp == npos) {
+                break;
+            }
+            ret = tmp;
+            pos = tmp + ref.size_;
+        } while(pos != this->size_);
+        return ret;
+    }
+
+    bool startsWith(StringRefBase ref) const {
+        return this->size_ >= ref.size_ && memcmp(this->ptr_, ref.ptr_, ref.size_) == 0;
+    }
+
+    bool endsWith(StringRefBase ref) const {
+        return this->size_ >= ref.size_ &&
+            memcmp(this->ptr_ + (this->size_ - ref.size_), ref.ptr_, ref.size_) == 0;
+    }
+};
+
+template <bool T>
+constexpr const typename StringRefBase<T>::size_type StringRefBase<T>::npos;
+
+using StringRef = StringRefBase<true>;
+
+inline bool operator==(StringRef left, StringRef right) {
+    return right.size() == right.size() && memcmp(left.data(), right.data(), left.size()) == 0;
+}
+
+inline bool operator!=(StringRef left, StringRef right) {
+    return !(left == right);
+}
+
+} // namespace ydsh
+
+namespace std {
+
+template <>
+struct hash<ydsh::StringRef> {
+    std::size_t operator()(const ydsh::StringRef &ref) const {
+        return ydsh::FNVHash64::compute(ref.begin(), ref.end());
+    }
+};
+
+} // namespace std
+
+#endif //YDSH_MISC_STRING_REF_HPP
---
+++ test/stringref/stringref_test.cpp
@@ -0,0 +1,142 @@
+#include "gtest/gtest.h"
+
+#include <misc/string_ref.hpp>
+
+using namespace ydsh;
+
+struct StringRefTest : public ::testing::Test {
+    template <size_t N>
+    void equals(const char (&expect)[N], const StringRef &ref) {
+        ASSERT_EQ(N - 1, ref.size());
+        ASSERT_TRUE(memcmp(expect, ref.data(), N - 1) == 0);
+    }
+};
+
+TEST_F(StringRefTest, base1) {
+    StringRef ref = "hello";
+    ASSERT_EQ(5UL, ref.size());
+    ASSERT_NO_FATAL_FAILURE(this->equals("hello", ref));
+
+    ref = StringRef("world!!", 4);
+    ASSERT_EQ(4UL, ref.size());
+    ASSERT_NO_FATAL_FAILURE(this->equals("worl", ref));
+}
+
+TEST_F(StringRefTest, base2) {
+    StringRef ref;
+    ASSERT_EQ(0UL, ref.size());
+    ASSERT_TRUE(ref.empty());
+    ASSERT_NO_FATAL_FAILURE(this->equals("", ref));
+
+    StringRef ref2 = ref;
+    ASSERT_TRUE(ref2.empty());
+    ASSERT_NO_FATAL_FAILURE(this->equals("", ref2));
+
+    ref2 = "!!!";
+    ASSERT_EQ(3UL, ref2.size());
+    ASSERT_NO_FATAL_FAILURE(this->equals("!!!", ref2));
+}
+
+TEST_F(StringRefTest, base3) {
+    std::string value = "12";
+    value += '\0';
+    value += "34";
+
+    StringRef ref = value;
+    ASSERT_EQ(5UL, ref.size());
+    ASSERT_NO_FATAL_FAILURE(this->equals({'1', '2', '\0', '3', '4', '\0'}, ref));
+    ASSERT_EQ(value.c_str(), ref.data());
+
+    auto *ptr = ref.take();
+    ASSERT_EQ(value.c_str(), ptr);
+    ASSERT_EQ(0UL, ref.size());
+    ASSERT_EQ(nullptr, ref.data());
+}
+
+TEST_F(StringRefTest, get) {
+    StringRef ref = "ABCD";
+    ASSERT_EQ('A', ref[0]);
+    ASSERT_EQ('B', ref[1]);
+    ASSERT_EQ('C', ref[2]);
+    ASSERT_EQ('D', ref[3]);
+
+    ASSERT_EQ('A', ref.front());
+    ASSERT_EQ('D', ref.back());
+
+    ASSERT_EQ('A', *ref.begin());
+    ASSERT_EQ('B', *(ref.begin() + 1));
+    ASSERT_EQ('C', *(ref.begin() + 2));
+    ASSERT_EQ('D', *(ref.begin() + 3));
+    ASSERT_EQ(ref.end(), ref.begin() + 4);
+
+    ASSERT_EQ('D', *(ref.end() - 1));
+    ASSERT_EQ('C', *(ref.end() - 2));
+    ASSERT_EQ('B', *(ref.end() - 3));
+    ASSERT_EQ('A', *(ref.end() - 4));
+}
+
+TEST_F(StringRefTest, comp) {
+    StringRef ref1 = "abc";
+    std::string value = "a";
+    value += "b";
+    value += "c";
+    StringRef ref2 = value;
+
+    ASSERT_TRUE(ref1 == ref2);
+    ASSERT_TRUE(ref2 == ref1);
+    ASSERT_TRUE(ref1.compare(ref2) == 0);
+    ASSERT_TRUE(ref2.compare(ref1) == 0);
+
+    ref2 = "f";
+    ASSERT_TRUE(ref1 != ref2);
+    ASSERT_TRUE(ref2 != ref1);
+    ASSERT_TRUE(ref1.compare(ref2) < 0);
+    ASSERT_TRUE(ref2.compare(ref1) > 0);
+
+    ref1 = "zzz";
+    ref2 = "dcfef";
+    ASSERT_TRUE(ref1.compare(ref2) > 0);
+    ASSERT_TRUE(ref2.compare(ref1) < 0);
+
+    ref1 = "123";
+    ref2 = "456";
+    ASSERT_TRUE(ref1.compare(ref2) < 0);
+    ASSERT_TRUE(ref2.compare(ref1) != 0);
+    ASSERT_TRUE(ref2.compare(ref1) > 0);
+
+    ref1 = "";
+    ref2 = "ADC";
+    ASSERT_TRUE(ref1.compare(ref2) < 0);
+    ASSERT_TRUE(ref2.compare(ref1) > 0);
+
+    ref1 = "";
+    ref2 = std::string();
+    ASSERT_TRUE(ref1 == ref2);
+    ASSERT_TRUE(ref1.compare(ref2) == 0);
+}
+
+TEST_F(StringRefTest, slice) {
+    StringRef ref = "hello world!!";
+    ASSERT_EQ("he", ref.slice(0, 2));
+    ASSERT_EQ("", ref.slice(0, 0));
+    ASSERT_EQ("world", ref.slice(6, 11));
+    ASSERT_EQ("world!!", ref.slice(6, 13));
+
+    ASSERT_TRUE(ref.startsWith(ref.slice(0, 2)));
+    ASSERT_FALSE(ref.startsWith(ref.slice(1, 2)));
+    ASSERT_TRUE(ref.endsWith(ref.slice(7, 13)));
+    ASSERT_FALSE(ref.endsWith(ref.slice(1, 2)));
+}
+
+TEST_F(StringRefTest, find) {
+    StringRef ref = "hello world!!";
+    ASSERT_EQ(0, ref.indexOf("h"));
+    ASSERT_EQ(2, ref.indexOf("ll"));
+    ASSERT_EQ(ref.size() - 1, ref.lastIndexOf("!"));
+    ASSERT_EQ(StringRef::npos, ref.lastIndexOf("?"));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
