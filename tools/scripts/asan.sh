03c99e35ab53ddda09fb48f6cc7d05ccd0d7ac87
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2020-03-14 19:59:31 +0900

    update asan.sh

--- tools/scripts/asan.sh
+++ tools/scripts/asan.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+CC=$1
+if [ -z $CC ]; then
+  CC=/usr/bin/clang++
+fi
+
 SCRIPT_DIR="$(cd $(dirname $0) && pwd -P)"
 ROOT=$SCRIPT_DIR/../..
 
 mkdir -p build-asan/app
 cd build-asan
 cmake $ROOT -G Ninja \
-            -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
+            -DCMAKE_CXX_COMPILER=$CC \
             -DCMAKE_BUILD_TYPE=debug \
             -DSANITIZER=address \
             -DCMAKE_INSTALL_PREFIX=./app
