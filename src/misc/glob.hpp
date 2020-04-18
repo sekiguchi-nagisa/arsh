a13ac337d9c351ede009017b7458eb2033d51bf0
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2020-04-18 23:37:59 +0900

    add basic functionality of glob, but need more work

--- src/misc/glob.hpp
+++ src/misc/glob.hpp
@@ -23,78 +23,211 @@
 #include <vector>
 
 #include "files.h"
+#include "flag_util.hpp"
 
 namespace ydsh {
-namespace __detail_glob {
+
+enum class WildMatchOption {
+    DOTGLOB = 1u << 0u, // match file names start with '.'
+};
+
+template <> struct allow_enum_bitop<WildMatchOption> : std::true_type {};
+
+enum class WildMatchResult {
+    FAILED,     // match failed
+    DOT,        // match '.'
+    DOTDOT,     // match '..'
+    MATCHED,    // match pattern
+};
 
 /**
- * based on (https://www.codeproject.com/Articles/1088/Wildcard-string-compare-globbing)
  *
- * @tparam Iter
  * @tparam Meta
  * detect meta character `?`, `*`
- * @param name
- * must be null terminated
- * @param iter
- * @param end
- * @return
- * if match pattern, return true.
+ * @tparam Iter
+ * pattern iterator
  */
 template <typename Meta, typename Iter>
-inline bool matchPattern(const char *name, Iter iter, Iter end) {
-    const char *cp = nullptr;
-    auto oldIter = end;
+class WildCardMatcher {
+private:
+    Iter iter;
+    Iter end;
 
-    while(*name) {
-        if(iter == end) {
-            return false;
-        } else if(Meta::isZeroOrMore(iter)) {
-            break;
-        } else if(*name != *iter && !Meta::isAny(iter)) {
-            return false;
-        }
-        ++name;
-        ++iter;
+    WildMatchOption option;
+
+public:
+    WildCardMatcher(Iter begin, Iter end, WildMatchOption option) :
+            iter(begin), end(end), option(option) {}
+
+    Iter getIter() const {
+        return this->iter;
     }
 
-    while(*name) {
-        if(iter == end) {
-            iter = oldIter;
-            name = cp++;
-        } else if(*name == *iter || Meta::isAny(iter)) {
+    bool isEnd() const {
+        return this->iter == this->end;
+    }
+
+    unsigned int consumeSep() {
+        unsigned int count = 0;
+        for(; !this->isEnd() && *this->iter == '/'; ++this->iter) {
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * based on (https://www.codeproject.com/Articles/1088/Wildcard-string-compare-globbing)
+     *
+     * @param name
+     * must be null terminated
+     * @return
+     */
+    WildMatchResult operator()(const char *name) {
+        const char *cp = nullptr;
+        auto oldIter = this->end;
+
+        // ignore starting with '.'
+        if(*name == '.') {
+            if(!this->isEndOrSep() && *this->iter != '.') {
+                if(!hasFlag(this->option, WildMatchOption::DOTGLOB)) {
+                    return WildMatchResult::FAILED;
+                }
+            }
+        }
+
+        while(*name) {
+            if(this->isEndOrSep()) {
+                return WildMatchResult::FAILED;
+            } else if(this->matchDot()) {
+                return WildMatchResult::DOT;
+            } else if(this->matchDotDot()) {
+                return WildMatchResult::DOTDOT;
+            } else if(Meta::isZeroOrMore(this->iter)) {
+                break;
+            } else if(*name != *this->iter && !Meta::isAny(this->iter)) {
+                return WildMatchResult::FAILED;
+            }
             ++name;
-            ++iter;
-        } else if(Meta::isZeroOrMore(iter)) {
-            if(++iter == end) {
+            ++this->iter;
+        }
+
+        while(*name) {
+            if(this->isEndOrSep()) {
+                this->iter = oldIter;
+                name = cp++;
+            } else if(*name == *this->iter || Meta::isAny(this->iter)) {
+                ++name;
+                ++this->iter;
+            } else if(Meta::isZeroOrMore(this->iter)) {
+                ++this->iter;
+                if(this->isEndOrSep()) {
+                    return WildMatchResult::MATCHED;
+                }
+                oldIter = this->iter;
+                cp = name + 1;
+            } else {
+                this->iter = oldIter;
+                name = cp++;
+            }
+        }
+
+        for(; !this->isEndOrSep() && Meta::isZeroOrMore(this->iter); this->iter++);
+        return this->isEndOrSep() ? WildMatchResult::MATCHED : WildMatchResult::FAILED;
+    }
+
+private:
+    bool isEndOrSep() const {
+        return this->isEnd() || *this->iter == '/';
+    }
+
+    bool matchDot() {
+        auto old = this->iter;
+        if(*this->iter == '.') {
+            ++this->iter;
+            if(this->isEndOrSep()) {
                 return true;
             }
-            oldIter = iter;
-            cp = name + 1;
-        } else {
-            iter = oldIter;
-            name = cp++;
         }
+        this->iter = old;
+        return false;
     }
 
-    for(; iter != end && Meta::isZeroOrMore(iter); iter++);
-    return iter == end;
-}
-
-} // namespace __detail_glob
-
-// for testing
-struct StrMetaChar {
-    static bool isAny(const char *iter) {
-        return *iter == '?';
-    }
-
-    static bool isZeroOrMore(const char *iter) {
-        return *iter == '*';
+    bool matchDotDot() {
+        auto old = this->iter;
+        if(*this->iter == '.') {
+            ++this->iter;
+            if(*this->iter == '.') {
+                ++this->iter;
+                if(this->isEndOrSep()) {
+                    return true;
+                }
+            }
+        }
+        this->iter = old;
+        return false;
     }
 };
 
-inline bool matchWildcard(const char *name, const char *pattern) {
-    return __detail_glob::matchPattern<StrMetaChar>(name, pattern, pattern + strlen(pattern));
+template <typename Meta, typename Iter>
+inline auto createWildCardMatcher(Iter begin, Iter end, WildMatchOption option) {
+    return WildCardMatcher<Meta, Iter>(begin, end, option);
+}
+
+template <typename Meta, typename Iter, typename Appender>
+unsigned int globBase(const char *baseDir, Iter iter, Iter end,
+                            Appender &appender, WildMatchOption option) {
+    DIR *dir = opendir(baseDir);
+    if(!dir) {
+        return 0;
+    }
+
+    unsigned int matchCount = 0;
+    for(dirent *entry; (entry = readdir(dir)); ) {
+        if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        auto matcher = createWildCardMatcher<Meta>(iter, end, option);
+        const WildMatchResult ret = matcher(entry->d_name);
+        if(ret == WildMatchResult::FAILED) {
+            continue;
+        }
+
+        std::string name = strcmp(baseDir, ".") != 0 ? baseDir : "";
+        if(!name.empty() && name.back() != '/') {
+            name += '/';
+        }
+        switch(ret) {
+        case WildMatchResult::DOT:
+            name += ".";
+            break;
+        case WildMatchResult::DOTDOT:
+            name += "..";
+            break;
+        default:
+            name += entry->d_name;
+            break;
+        }
+
+        if(S_ISDIR(getStMode(name.c_str()))) {
+            if(matcher.consumeSep() > 0) {
+                name += '/';
+            }
+            if(!matcher.isEnd()) {
+                matchCount += globBase<Meta>(name.c_str(), matcher.getIter(), end, appender, option);
+            }
+        }
+        if(matcher.isEnd()) {
+            appender(std::move(name));
+            matchCount++;
+        }
+
+        if(ret == WildMatchResult::DOT || ret == WildMatchResult::DOTDOT) {
+            break;
+        }
+    }
+    closedir(dir);
+    return matchCount;
 }
 
 } // namespace ydsh
--- test/glob/glob_test.cpp
+++ test/glob/glob_test.cpp
@@ -1,50 +1,255 @@
+#include <string>
+#include <algorithm>
+
 #include "gtest/gtest.h"
 
-#include "../test_common.h"
 #include <misc/glob.hpp>
 #include <misc/string_ref.hpp>
 
+#ifndef GLOB_TEST_WORK_DIR
+#error "require EXEC_TEST_DIR"
+#endif
+
 using namespace ydsh;
 
-class GlobTest : public ::testing::Test, public TempFileFactory {
-protected:
-    std::string oldCWD;
+// for testing
+struct StrMetaChar {
+    static bool isAny(const char *iter) {
+        return *iter == '?';
+    }
 
-    int visitTempRoot() {
-        char *ptr = realpath(".", nullptr);
-        this->oldCWD = ptr;
-        free(ptr);
-        errno = 0;
-        return chdir(this->tmpDirName.c_str());
+    static bool isZeroOrMore(const char *iter) {
+        return *iter == '*';
     }
 };
 
-static bool matchPattern(const char *name, const char *p) {
-    return matchWildcard(name, p);
+static bool matchPattern(const char *name, const char *p, WildMatchOption option = {}) {
+    auto matcher = createWildCardMatcher<StrMetaChar>(p, p + strlen(p), option);
+    return matcher(name) != WildMatchResult::FAILED;
 }
 
-TEST_F(GlobTest, pattern) {
+class GlobTest : public ::testing::Test {
+public:
+    static constexpr const char *workdir = GLOB_TEST_WORK_DIR;
+
+    GlobTest() {
+        if(chdir(workdir) == -1) {
+            fatal_perror("broken directory: %s", GLOB_TEST_WORK_DIR);
+        }
+    }
+};
+
+class Appender {
+private:
+    std::vector<std::string> paths;
+
+public:
+    void operator()(std::string &&path) {
+        this->paths.push_back(std::move(path));
+    }
+
+    std::vector<std::string> take() {
+        std::sort(this->paths.begin(), this->paths.end());
+        return std::move(this->paths);
+    }
+};
+
+TEST_F(GlobTest, pattern1) {
     ASSERT_FALSE(matchPattern("", "ff"));
     ASSERT_FALSE(matchPattern("", "?"));
     ASSERT_TRUE(matchPattern("", "*"));
+    ASSERT_TRUE(matchPattern("", "*/"));
     ASSERT_TRUE(matchPattern("", ""));
+    ASSERT_TRUE(matchPattern("", "/"));
     ASSERT_TRUE(matchPattern("hoge", "hoge"));
+    ASSERT_TRUE(matchPattern("hoge", "hoge/"));
     ASSERT_FALSE(matchPattern("hoge", ""));
     ASSERT_FALSE(matchPattern("hoge", "h"));
     ASSERT_FALSE(matchPattern("hoge", "wwww"));
     ASSERT_FALSE(matchPattern("hoge", "h*g"));
     ASSERT_TRUE(matchPattern("hoge", "*h*ge"));
+    ASSERT_TRUE(matchPattern("hoge", "*h*ge/"));
     ASSERT_FALSE(matchPattern("hoge", "?"));
     ASSERT_TRUE(matchPattern("hoge", "????"));
     ASSERT_TRUE(matchPattern("hoge", "?*"));
     ASSERT_TRUE(matchPattern("hoge", "*"));
+    ASSERT_TRUE(matchPattern("hoge", "*/"));
     ASSERT_FALSE(matchPattern("hoge", "h*gw"));
     ASSERT_FALSE(matchPattern("hoge", "g*w"));
     ASSERT_TRUE(matchPattern("hoge", "h*ge*"));
     ASSERT_TRUE(matchPattern("hoge", "h**g**e*"));
+    ASSERT_TRUE(matchPattern("hoge", "h**g**e*/A/A"));
     ASSERT_TRUE(matchPattern("hoge", "h*ge**"));
     ASSERT_FALSE(matchPattern("hoge", "h*ge?"));
     ASSERT_FALSE(matchPattern("hoge", "h*ge**?"));
+    ASSERT_FALSE(matchPattern("hoge", "h*ge**?/"));
+    ASSERT_TRUE(matchPattern("hoge", "h*ge**/AAA"));
+}
+
+TEST_F(GlobTest, pattern2) {
+    ASSERT_TRUE(matchPattern(".", "."));
+    ASSERT_TRUE(matchPattern(".", ".", WildMatchOption::DOTGLOB));
+    ASSERT_FALSE(matchPattern(".", "*"));
+    ASSERT_TRUE(matchPattern(".", "*", WildMatchOption::DOTGLOB));
+    ASSERT_FALSE(matchPattern(".", "?"));
+    ASSERT_TRUE(matchPattern(".", "?", WildMatchOption::DOTGLOB));
+    ASSERT_FALSE(matchPattern("..", "*"));
+    ASSERT_TRUE(matchPattern("..", "*", WildMatchOption::DOTGLOB));
+    ASSERT_FALSE(matchPattern("..", "*?"));
+    ASSERT_TRUE(matchPattern("..", "*?", WildMatchOption::DOTGLOB));
+    ASSERT_FALSE(matchPattern("..", "?*"));
+    ASSERT_TRUE(matchPattern("..", "?*", WildMatchOption::DOTGLOB));
+    ASSERT_FALSE(matchPattern(".hoge", "*?"));
+    ASSERT_TRUE(matchPattern(".hoge", "*?", WildMatchOption::DOTGLOB));
+    ASSERT_TRUE(matchPattern("..", ".*?"));
+    ASSERT_TRUE(matchPattern("..", ".*?", WildMatchOption::DOTGLOB));
+    ASSERT_TRUE(matchPattern(".hoge", ".?*"));
+    ASSERT_TRUE(matchPattern(".hoge", ".?*", WildMatchOption::DOTGLOB));
+}
+
+static std::vector<std::string> testGlobBase(const char *dir,
+                                const char *pattern, WildMatchOption option = {}) {
+    Appender appender;
+    globBase<StrMetaChar>(dir, pattern, pattern + strlen(pattern), appender, option);
+    return appender.take();
+}
+
+// test `globBase' api
+
+TEST_F(GlobTest, base_invalid) {    // invalid base dir
+    auto ret = testGlobBase("", "");
+    ASSERT_TRUE(ret.empty());
+
+    ret = testGlobBase("", "*");
+    ASSERT_TRUE(ret.empty());
+
+    ret = testGlobBase("", "AAA");
+    ASSERT_TRUE(ret.empty());
+
+    ret = testGlobBase("hfuierhtfnv", "*");
+    ASSERT_TRUE(ret.empty());
+}
+
+TEST_F(GlobTest, base_fileOrDir1) {    // match file or dir
+    auto ret = testGlobBase(".", "*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ("AAA", ret[0]);
+    ASSERT_EQ("bbb", ret[1]);
+
+    ret = testGlobBase("./", "*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ("./AAA", ret[0]);
+    ASSERT_EQ("./bbb", ret[1]);
+
+    ret = testGlobBase(".//", "*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ(".//AAA", ret[0]);
+    ASSERT_EQ(".//bbb", ret[1]);
+
+    ret = testGlobBase(".///", "*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ(".///AAA", ret[0]);
+    ASSERT_EQ(".///bbb", ret[1]);
+
+    ret = testGlobBase("././/.//", "*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ("././/.//AAA", ret[0]);
+    ASSERT_EQ("././/.//bbb", ret[1]);
+
+    ret = testGlobBase("././/.//", "*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ("././/.//AAA", ret[0]);
+    ASSERT_EQ("././/.//bbb", ret[1]);
+
+    ret = testGlobBase(".", "*/../*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ("bbb/../AAA", ret[0]);
+    ASSERT_EQ("bbb/../bbb", ret[1]);
+
+    ret = testGlobBase(".", "*/./*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ("bbb/./AA21", ret[0]);
+    ASSERT_EQ("bbb/./b21", ret[1]);
+
+    ret = testGlobBase(".", "*/./.?*");
+    ASSERT_EQ(1, ret.size());
+    ASSERT_EQ("bbb/./.hidden", ret[0]);
+
+    ret = testGlobBase(".", "*/./*", WildMatchOption::DOTGLOB);
+    ASSERT_EQ(3, ret.size());
+    ASSERT_EQ("bbb/./.hidden", ret[0]);
+    ASSERT_EQ("bbb/./AA21", ret[1]);
+    ASSERT_EQ("bbb/./b21", ret[2]);
+}
+
+TEST_F(GlobTest, base_fileOrDir2) {    // match file or dir
+    auto ret = testGlobBase(".", "bbb/*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ("bbb/AA21", ret[0]);
+    ASSERT_EQ("bbb/b21", ret[1]);
+
+    ret = testGlobBase(".", "./././bbb/?*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ("./././bbb/AA21", ret[0]);
+    ASSERT_EQ("./././bbb/b21", ret[1]);
+
+    ret = testGlobBase("./", "*//*/*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ("./bbb/b21/A321", ret[0]);
+    ASSERT_EQ("./bbb/b21/D", ret[1]);
+}
+
+TEST_F(GlobTest, base_onlyDir) {    // match only dir
+    auto ret = testGlobBase("./", "*/");
+    ASSERT_EQ(1, ret.size());
+    ASSERT_EQ("./bbb/", ret[0]);
+
+    ret = testGlobBase(".", "*//");
+    ASSERT_EQ(1, ret.size());
+    ASSERT_EQ("bbb/", ret[0]);
+
+    ret = testGlobBase(".", "*////");
+    ASSERT_EQ(1, ret.size());
+    ASSERT_EQ("bbb/", ret[0]);
+
+    ret = testGlobBase(".", "*/.");
+    ASSERT_EQ(1, ret.size());
+    ASSERT_EQ("bbb/.", ret[0]);
+
+    ret = testGlobBase(".", "*/..");
+    ASSERT_EQ(1, ret.size());
+    ASSERT_EQ("bbb/..", ret[0]);
+
+    ret = testGlobBase("./", "*//.//..");
+    ASSERT_EQ(1, ret.size());
+    ASSERT_EQ("./bbb/./..", ret[0]);
+
+    ret = testGlobBase(".", "*//*/");
+    ASSERT_EQ(1, ret.size());
+    ASSERT_EQ("bbb/b21/", ret[0]);
+
+    ret = testGlobBase(".", "*//*/*/");
+    ASSERT_TRUE(ret.empty());
+
+    ret = testGlobBase(".", "*//*./");
+    ASSERT_TRUE(ret.empty());
+}
+
+TEST_F(GlobTest, base_fullpath) {
+    auto ret = testGlobBase(this->workdir, "bbb/*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ(GLOB_TEST_WORK_DIR "/bbb/AA21", ret[0]);
+    ASSERT_EQ(GLOB_TEST_WORK_DIR "/bbb/b21", ret[1]);
+
+    ret = testGlobBase(this->workdir, "../*/bbb/*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ(GLOB_TEST_WORK_DIR "/../dir/bbb/AA21", ret[0]);
+    ASSERT_EQ(GLOB_TEST_WORK_DIR "/../dir/bbb/b21", ret[1]);
+
+    ret = testGlobBase(this->workdir, "../*/*/*");
+    ASSERT_EQ(2, ret.size());
+    ASSERT_EQ(GLOB_TEST_WORK_DIR "/../dir/bbb/AA21", ret[0]);
+    ASSERT_EQ(GLOB_TEST_WORK_DIR "/../dir/bbb/b21", ret[1]);
 }
 
 int main(int argc, char **argv) {
