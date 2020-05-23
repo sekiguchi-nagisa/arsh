6d83f4b379c6f85b665e27ec3cfdb2a2a70aa1d3
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2020-05-23 20:19:14 +0900

    change operator precedence of throw expression

--- src/token_kind.h
+++ src/token_kind.h
@@ -200,11 +200,11 @@
     OP(UNMATCH   , 10, INFIX) \
     OP(STR_CHECK , 10, INFIX) \
     OP(WITH      , 9, INFIX) \
-    OP(THROW     , 8, PREFIX|RASSOC) \
     OP(PIPE      , 7, INFIX) \
     OP(COND_AND  , 6, INFIX) \
     OP(COND_OR   , 5, INFIX) \
     OP(TERNARY   , 4, INFIX) \
+    OP(THROW     , 3, PREFIX|RASSOC) \
     OP(COPROC    , 3, PREFIX|RASSOC) \
     OP(BACKGROUND, 2, INFIX) \
     OP(DISOWN_BG , 2, INFIX) \
