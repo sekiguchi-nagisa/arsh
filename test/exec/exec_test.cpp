683b6127ac53f6d3c3e49ed2d2ae07d3fc671f93
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2020-07-05 17:07:10 +0900

    ignore invalid test script from exec_test

--- test/exec/exec_test.cpp
+++ test/exec/exec_test.cpp
@@ -4,6 +4,7 @@
 
 #include <ydsh/ydsh.h>
 #include <misc/files.h>
+#include <misc/string_ref.hpp>
 #include <directive.h>
 
 #include "../test_common.h"
@@ -34,6 +35,9 @@ int parse(const char *src, T&& ...args) {
 static std::vector<std::string> getSortedFileList(const char *dir) {
     auto ret = getFileList(dir, true);
     assert(!ret.empty());
+    ret.erase(std::remove_if(ret.begin(), ret.end(), [](const std::string &v) {
+        return !StringRef(v).endsWith(".ds");
+    }), ret.end());
     std::sort(ret.begin(), ret.end());
     ret.erase(std::unique(ret.begin(), ret.end()), ret.end());
     return ret;
