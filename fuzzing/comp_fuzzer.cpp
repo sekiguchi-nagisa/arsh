bae9f92ff19b878b7d4aebbac96d5db5c3d4911f
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2021-02-10 22:40:52 +0900

    add fuzzer for completion

---
+++ fuzzing/comp_fuzzer.cpp
@@ -0,0 +1,18 @@
+//
+// Created by skgchxngsxyz-leap on 2021/02/10.
+//
+
+#include <stdint.h>
+#include <stddef.h>
+#include <string>
+
+#include <ydsh/ydsh.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    auto *state = DSState_createWithMode(DS_EXEC_MODE_COMPILE_ONLY);
+    std::string buf((const char *)data, size);
+    const char *ptr = buf.c_str();
+    DSState_complete(state, DS_COMP_INVOKE, size, &ptr);
+    DSState_delete(&state);
+    return 0;
+}
