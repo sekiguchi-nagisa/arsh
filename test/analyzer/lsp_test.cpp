c9e158de620bd631eaaeb9d53c602f2344e68813
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2021-07-05 23:19:29 +0900

    update test case

--- test/analyzer/lsp_test.cpp
+++ test/analyzer/lsp_test.cpp
@@ -387,6 +387,15 @@ TEST_F(ServerTest, term3) {
 
 TEST(ASTCtxTest, base) {
   auto ctx = std::make_unique<ASTContext>(1, uri::URI::fromString("file:///file"), "#hello", 1);
+  auto *handle = ctx->getScope()->find("COMP_HOOK");
+  ASSERT_TRUE(handle);
+  ASSERT_TRUE(hasFlag(handle->attr(), FieldAttribute::GLOBAL));
+  ASSERT_EQ(0, handle->getModID());
+  handle = ctx->getScope()->find("TRUE");
+  ASSERT_TRUE(handle);
+  ASSERT_TRUE(hasFlag(handle->attr(), FieldAttribute::GLOBAL | FieldAttribute::READ_ONLY));
+  ASSERT_EQ(0, handle->getModID());
+  ASSERT_TRUE(ctx->getPool().getDiscardPoint().typeIdOffset <= UINT8_MAX);
 }
 
 int main(int argc, char **argv) {
