64e66c676afafe3229db098b4684575d7b009549
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2021-08-01 15:27:10 +0900

    fix build error in fuzzing build

--- fuzzing/rpc_fuzzer.cpp
+++ fuzzing/rpc_fuzzer.cpp
@@ -7,13 +7,18 @@
 
 #include "jsonrpc.h"
 
+struct NullLogger : public ydsh::LoggerBase {
+  NullLogger() : LoggerBase("") {}
+};
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   using namespace ydsh;
   using namespace rpc;
 
+  ::NullLogger logger;
   ByteBuffer buf;
   buf.append(reinterpret_cast<const char *>(data), size);
-  MessageParser parser(std::move(buf));
+  MessageParser parser(logger, std::move(buf));
   auto ret = parser();
   if (parser.hasError()) {
     FILE *fp = fopen("/dev/null", "w");
