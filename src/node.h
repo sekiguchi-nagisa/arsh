173d213e084c08fc0e738ae598ca62e2dcc7bb80
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2023-02-23 21:45:18 +0900

    fix build error

--- src/node.h
+++ src/node.h
@@ -176,7 +176,7 @@ public:
 template <typename T, NodeKind K, enable_when<std::is_base_of<Node, T>::value> = nullptr>
 class WithRtti : public T {
 protected:
-  static_assert(sizeof(Node) == (8 + 24 + sizeof(uintptr_t)));
+  static_assert(sizeof(Node) == (24 + sizeof(uintptr_t) * 2));
 
   explicit WithRtti(Token token) : T(K, token) {}
 
