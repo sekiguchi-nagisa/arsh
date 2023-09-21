194701fc06a74b08eac3c7b4818ea0bc5d96d3be
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2023-09-21 22:32:23 +0900

    update re2c to v3.1

--- cmake/modules/GetRE2C.cmake
+++ cmake/modules/GetRE2C.cmake
@@ -5,42 +5,42 @@ include(Helper)
 macro(getRE2C)
     if (CMAKE_VERSION VERSION_LESS 3.2)
         set(UPDATE_DISCONNECTED_IF_AVAILABLE "")
-    else()
+    else ()
         set(UPDATE_DISCONNECTED_IF_AVAILABLE "UPDATE_DISCONNECTED 1")
-    endif()
+    endif ()
 
-    if(CMAKE_VERSION VERSION_LESS 3.12)
+    if (CMAKE_VERSION VERSION_LESS 3.12)
         set(BUILD_RE2C_WITH_CMAKE OFF)
         check_program(autoreconf)
         check_program(aclocal)
-    else()
+    else ()
         set(BUILD_RE2C_WITH_CMAKE ON)
-    endif()
+    endif ()
 
     download_project(
-            PROJ                re2c
-            GIT_REPOSITORY      https://github.com/skvadrik/re2c.git
-            GIT_TAG             3.0
-            GIT_PROGRESS        1
+            PROJ re2c
+            GIT_REPOSITORY https://github.com/skvadrik/re2c.git
+            GIT_TAG 3.1
+            GIT_PROGRESS 1
             ${UPDATE_DISCONNECTED_IF_AVAILABLE}
     )
 
     set(RE2C_SRC "${re2c_SOURCE_DIR}")
     set(RE2C_BIN "${re2c_BINARY_DIR}/re2c")
 
-    if(BUILD_RE2C_WITH_CMAKE)
+    if (BUILD_RE2C_WITH_CMAKE)
         execute_process(COMMAND ${CMAKE_COMMAND} -DCMAKE_CXX_FLAGS=-D_GNU_SOURCE -G "${CMAKE_GENERATOR}" ${RE2C_SRC} WORKING_DIRECTORY ${re2c_BINARY_DIR})
         execute_process(COMMAND ${CMAKE_COMMAND} --build . WORKING_DIRECTORY ${re2c_BINARY_DIR})
-    else()
-        if(NOT EXISTS "${RE2C_SRC}/configure")
+    else ()
+        if (NOT EXISTS "${RE2C_SRC}/configure")
             execute_process(COMMAND ./autogen.sh WORKING_DIRECTORY ${RE2C_SRC})
-        endif()
+        endif ()
 
         execute_process(COMMAND ${RE2C_SRC}/configure WORKING_DIRECTORY ${re2c_BINARY_DIR})
         execute_process(COMMAND make WORKING_DIRECTORY ${re2c_BINARY_DIR})
-    endif()
+    endif ()
 
-    if(NOT EXISTS "${RE2C_BIN}")
+    if (NOT EXISTS "${RE2C_BIN}")
         message(FATAL_ERROR "re2c is not found")
-    endif()
+    endif ()
 endmacro()
