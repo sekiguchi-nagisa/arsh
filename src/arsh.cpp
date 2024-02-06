31107589e6e2792ff3a9f7d76e3dd1ae2e15a891
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2024-02-07 02:50:45 +0900

    Revert "always call waitForAny api before read line"
    This reverts commit 803bd1e990e91960bf1d08f2c1a593d8eae8e966.

--- src/arsh.cpp
+++ src/arsh.cpp
@@ -574,7 +574,6 @@ static const char *defaultPrompt() {
 ssize_t ARState_readLine(ARState *st, char *buf, size_t bufSize, ARError *e) {
   GUARD_NULL(st, 0);
   st->getCallStack().clearThrownObject();
-  st->jobTable.waitForAny();
   st->notifyCallback.showAndClear();
   if (e) {
     *e = initARError();
