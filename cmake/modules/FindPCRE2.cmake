7bf471b55aa1e9075401ce33ae069fbb83867a89
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2024-03-12 00:41:07 +0900

    fix pcre2 detection in macOS homebrew

--- cmake/modules/FindPCRE2.cmake
+++ cmake/modules/FindPCRE2.cmake
@@ -2,7 +2,8 @@
 # for pcre2 detection
 
 find_path(PCRE2_INCLUDE_DIR NAMES "pcre2.h")
-set(CMAKE_LIBRARY_PATH "/usr/lib/x86_64-linux-gnu")
+set(CMAKE_INCLUDE_PATH "/usr/local/include;/opt/homebrew/include") # for macOS Homebrew
+set(CMAKE_LIBRARY_PATH "/usr/lib/x86_64-linux-gnu;/opt/homebrew/lib")
 find_library(PCRE2_LIBRARY NAMES "pcre2-8")
 
 if (PCRE2_INCLUDE_DIR)
@@ -24,4 +25,4 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(PCRE2
         REQUIRED_VARS PCRE2_LIBRARY PCRE2_INCLUDE_DIR
         VERSION_VAR PCRE2_VERSION
-        )
+)
