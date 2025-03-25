f721acdd7dd0ba264395e51d2fe6f19158de85dc
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2025-03-25 21:54:05 +0900

    use commit hash for download project

--- cmake/modules/GetRE2C.cmake
+++ cmake/modules/GetRE2C.cmake
@@ -20,7 +20,7 @@ macro(getRE2C)
     download_project(
             PROJ re2c
             GIT_REPOSITORY https://github.com/skvadrik/re2c.git
-            GIT_TAG 4.1
+            GIT_TAG 8baaca5774db8b83784afcbfcc3fe761a1fcbd37 # 4.1
             GIT_PROGRESS 1
             ${UPDATE_DISCONNECTED_IF_AVAILABLE}
     )
