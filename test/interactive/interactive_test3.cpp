86798ed44ab3d5abfc36ad0f2a134b9f61dd05f8
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2026-07-04 18:03:56 +0900

    adjust interactive test case

--- test/interactive/interactive_test3.cpp
+++ test/interactive/interactive_test3.cpp
@@ -754,13 +754,11 @@ TEST_F(InteractiveTest, changeTCPGRPInChild) {
   this->invoke("--quiet", "--norc");
 
   ASSERT_NO_FATAL_FAILURE(this->expect(PROMPT));
-  {
-    auto cleanup = this->withTimeout(500);
-    ASSERT_NO_FATAL_FAILURE(this->sendLineAndExpect(
-        "call $BIN_NAME -c 'shctl set monitor; ls > /dev/null;' &", ": Job = %1"));
-    std::string err = format("[warn] retry readLine, caused by `%s'\n", strerror(EIO));
-    ASSERT_NO_FATAL_FAILURE(this->sendLineAndExpect("", "", err.c_str()));
-  }
+  ASSERT_NO_FATAL_FAILURE(this->sendLineAndExpect(
+      "call $BIN_NAME -c 'shctl set monitor; ls > /dev/null;' &", ": Job = %1"));
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  std::string err = format("[warn] retry readLine, caused by `%s'\n", strerror(EIO));
+  ASSERT_NO_FATAL_FAILURE(this->sendLineAndExpect("", "", err.c_str()));
   ASSERT_NO_FATAL_FAILURE(this->sendLineAndWait("exit", 0));
 }
 
